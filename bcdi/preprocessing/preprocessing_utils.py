fc9e975062bc9169ee14e8de8b80d695ba540d46
Jerome Carnis <jerome.carnis@desy.de>
2019-10-14 11:17:23 +0200

    Bug: correct load_data to use the parameter setup.filtered_data with higher priority than setup.custom_scan

--- bcdi/preprocessing/preprocessing_utils.py
+++ bcdi/preprocessing/preprocessing_utils.py
@@ -820,6 +820,126 @@ def find_bragg(data, peak_method):
     return z0, y0, x0
 
 
+def grid_bcdi(logfile, scan_number, detector, setup, flatfield=None, hotpixels=None, orthogonalize=False, hxrd=None,
+              normalize=False, debugging=False, **kwargs):
+    """
+    Load the Bragg CDI data, apply filters and concatenate it for phasing.
+
+    :param logfile: file containing the information about the scan and image numbers (specfile, .fio...)
+    :param scan_number: the scan number to load
+    :param detector: the detector object: Class experiment_utils.Detector()
+    :param setup: the experimental setup: Class SetupPreprocessing()
+    :param flatfield: the 2D flatfield array
+    :param hotpixels: the 2D hotpixels array. 1 for a hotpixel, 0 for normal pixels.
+    :param orthogonalize: if True will interpolate the data and the mask on an orthogonal grid using xrayutilities
+    :param hxrd: an initialized xrayutilities HXRD object used for the orthogonalization of the dataset
+    :param normalize: set to True to normalize the diffracted intensity by the incident X-ray beam intensity
+    :param debugging: set to True to see plots
+    :param kwargs:
+     - follow_bragg (bool): True when for energy scans the detector was also scanned to follow the Bragg peak
+    :return:
+     - the 3D data array (in an orthonormal frame or in the detector frame) and the 3D mask array
+     - frames_logical: array of initial length the number of measured frames. In case of padding the length changes.
+       A frame whose index is set to 1 means that it is used, 0 means not used, -1 means padded (added) frame.
+     - the monitor values for normalization
+    """
+    for k in kwargs.keys():
+        if k in ['follow_bragg']:
+            follow_bragg = kwargs['follow_bragg']
+        else:
+            raise Exception("unknown keyword argument given: allowed is 'follow_bragg'")
+    if setup.rocking_angle == 'energy':
+        try:
+            follow_bragg
+        except NameError:
+            raise TypeError("Parameter 'follow_bragg' not provided, defaulting to False")
+    rawdata, rawmask, monitor, frames_logical = load_data(logfile=logfile, scan_number=scan_number, detector=detector,
+                                                          setup=setup, flatfield=flatfield, hotpixels=hotpixels,
+                                                          debugging=debugging)
+    # normalize by the incident X-ray beam intensity
+    if normalize:
+        rawdata, monitor, _ = normalize_dataset(array=rawdata, raw_monitor=monitor, frames_logical=frames_logical,
+                                                norm_to_min=True, debugging=debugging)
+
+    # bin data and mask in the detector plane if needed
+    # binning in the stacking dimension is done at the very end of the data processing
+    if (detector.binning[1] != 1) or (detector.binning[2] != 1):
+        rawdata = pu.bin_data(rawdata, (1, detector.binning[1], detector.binning[2]), debugging=False)
+        rawmask = pu.bin_data(rawmask, (1, detector.binning[1], detector.binning[2]), debugging=False)
+        rawmask[np.nonzero(rawmask)] = 1
+
+    if not orthogonalize:
+        return [], rawdata, [], rawmask, [], frames_logical, monitor
+
+    elif setup.is_orthogonal:
+        # load q values, the data is already orthogonalized
+        import tkinter as tk
+        from tkinter import filedialog
+
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(title="Select the file containing QxQzQy",
+                                               initialdir=detector.datadir, filetypes=[("NPZ", "*.npz")])
+        npzfile = np.load(file_path)
+        q_values = [npzfile['qx'], npzfile['qz'], npzfile['qy']]
+        return q_values, [], rawdata, [], rawmask, frames_logical, monitor
+
+    else:
+        if setup.filtered_data:
+            print('Trying to orthogonalize a filtered data, the corresponding detector ROI should be provided\n'
+                  'otherwise q values will be wrong.')
+        nbz, nby, nbx = rawdata.shape
+        qx, qz, qy, frames_logical = \
+            regrid(logfile=logfile, nb_frames=rawdata.shape[0], scan_number=scan_number, detector=detector,
+                   setup=setup, hxrd=hxrd, frames_logical=frames_logical, follow_bragg=follow_bragg)
+        # qx, qz, qy have the same shape as the data
+        if setup.beamline == 'ID01':
+            # below is specific to ID01 energy scans where frames are duplicated for undulator gap change
+            if setup.rocking_angle == 'energy':  # frames need to be removed
+                tempdata = np.zeros(((frames_logical != 0).sum(), nby, nbx))
+                offset_frame = 0
+                for idx in range(nbz):
+                    if frames_logical[idx] != 0:  # use frame
+                        tempdata[idx-offset_frame, :, :] = rawdata[idx, :, :]
+                    else:  # average with the precedent frame
+                        offset_frame = offset_frame + 1
+                        tempdata[idx-offset_frame, :, :] = (tempdata[idx-offset_frame, :, :] + rawdata[idx, :, :])/2
+                rawdata = tempdata
+                rawmask = rawmask[0:rawdata.shape[0], :, :]  # truncate the mask to have the correct size
+
+        # # create the grid for interpolation
+        # qx = np.linspace(old_qx.min(), old_qx.max(), nbz, endpoint=False)  # downstream at 0 sample angles
+        # qz = np.linspace(old_qz.min(), old_qz.max(), nby, endpoint=False)  # vertical up at 0 sample angles
+        # qy = np.linspace(old_qy.min(), old_qy.max(), nbx, endpoint=False)  # outboard at 0 sample angles
+        #
+        # # interpolate the data onto the new q values
+        # rgi = RegularGridInterpolator((cdi_angle * np.pi / 180, np.arange(-directbeam_y, -directbeam_y + nby, 1),
+        #                                np.arange(-directbeam_x, -directbeam_x + nbx, 1)),
+        #                               data, method='linear', bounds_error=False, fill_value=np.nan)
+        # newdata = rgi(np.concatenate((angle_det.reshape((1, z_interp.size)),
+        #                               y_det.reshape((1, z_interp.size)),
+        #                               x_det.reshape((1, z_interp.size)))).transpose())
+        # newdata = newdata.reshape((numz, numy, numx)).astype(data.dtype)
+
+
+        gridder = xu.Gridder3D(nbz, nby, nbx)
+        # convert mask to rectangular grid in reciprocal space
+        gridder(qx, qz, qy, rawmask)
+        mask = np.copy(gridder.data)
+        # convert data to rectangular grid in reciprocal space
+        gridder(qx, qz, qy, rawdata)
+
+        q_values = [gridder.xaxis, gridder.yaxis, gridder.zaxis]
+        fig, _, _ = gu.contour_slices(gridder.data, (gridder.xaxis, gridder.yaxis, gridder.zaxis), sum_frames=False,
+                                      title='Regridded data',
+                                      levels=np.linspace(0, int(np.log10(gridder.data.max())), 150, endpoint=False),
+                                      plot_colorbar=True, scale='log', is_orthogonal=True, reciprocal_space=True)
+        fig.savefig(detector.savedir + 'reciprocal_space_' + str(nbz) + '_' + str(nby) + '_' + str(nbx) + '_' + '.png')
+        plt.close(fig)
+
+        return q_values, rawdata, gridder.data, rawmask, mask, frames_logical, monitor
+
+
 def grid_cdi(logfile, scan_number, detector, setup, flatfield=None, hotpixels=None, orthogonalize=False,
              normalize=False, correct_curvature=False, debugging=False):
     """
@@ -1163,7 +1283,7 @@ def load_data(logfile, scan_number, detector, setup, flatfield=None, hotpixels=N
     if hotpixels is None:
         hotpixels = np.zeros((detector.nb_pixel_y, detector.nb_pixel_x))
 
-    if setup.custom_scan:
+    if setup.custom_scan and not setup.filtered_data:
         data, mask3d, monitor, frames_logical = load_custom_data(custom_images=setup.custom_images,
                                                                  custom_monitor=setup.custom_monitor,
                                                                  detector=detector, flatfield=flatfield,
