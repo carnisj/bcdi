f5b9446d0b10ccfc91aa306a1cedf5509bd9e74e
Jerome Carnis <carnis_jerome@yahoo.fr>
2019-10-15 15:36:17 +0200

    Feature: implement tick spacing / field of view (anisotropic possible but mayavi axes label spacing has to be identical for each axis)

--- bcdi/publication/scripts/template_mayavi_3d.py
+++ bcdi/publication/scripts/template_mayavi_3d.py
@@ -8,11 +8,12 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import mayavi
 from mayavi import mlab
 import tkinter as tk
 from tkinter import filedialog
 import sys
-sys.path.append('//win.desy.de/home/carnisj/My Documents/myscripts/bcdi/')
+sys.path.append('C:/Users/Jerome/Documents/myscripts/bcdi/')
 import bcdi.postprocessing.postprocessing_utils as pu
 import bcdi.graph.graph_utils as gu
 
@@ -21,16 +22,16 @@ Template for making 3d isosurface figures for paper
 Open an npz file (reconstruction ampdispstrain.npz) and save individual figures including a length scale.
 """
 
-scan = 1012    # spec scan number
-root_folder = "D:/data/HC3207/"
+scan = 978  # 1012    # spec scan number
+root_folder = "C:/Users/Jerome/Documents/data/HC3207/"  # ""D:/data/HC3207/"
 sample_name = "SN"  # "S"  #
 comment = ""
 
 voxel_size = 6.0  # in nm, supposed isotropic
 tick_spacing = 50  # for plots, in nm
-field_of_view = 400  # in nm, can be larger than the total width (the array will be padded)
+field_of_view = [700, 700, 700]  # [z,y,x] in nm, can be larger than the total width (the array will be padded)
 
-threshold_isosurface = 0.5
+threshold_isosurface = 0.35
 
 #############
 # load data #
@@ -50,7 +51,7 @@ if amp.ndim != 3:
 
 amp = amp / amp.max()
 amp[amp < threshold_isosurface] = 0
-amp[np.nonzero(amp)] = 1  # create a support for the isosurface
+# amp[np.nonzero(amp)] = 1  # create a support for the isosurface
 
 numz, numy, numx = amp.shape
 print("Initial data size: (", numz, ',', numy, ',', numx, ')')
@@ -59,8 +60,10 @@ print("Initial data size: (", numz, ',', numy, ',', numx, ')')
 #  pad arrays to obtain the desired field of view #
 ###################################################
 pixel_spacing = tick_spacing / voxel_size
-pixel_FOV = int(np.rint((field_of_view / voxel_size) / 2))  # half-number of pixels corresponding to the FOV
-new_shape = [max(numz, 2*pixel_FOV), max(numy, 2*pixel_FOV), max(numx, 2*pixel_FOV)]
+z_pixel_FOV = int(np.rint((field_of_view[0] / voxel_size) / 2))  # half-number of pixels corresponding to the FOV
+y_pixel_FOV = int(np.rint((field_of_view[1] / voxel_size) / 2))  # half-number of pixels corresponding to the FOV
+x_pixel_FOV = int(np.rint((field_of_view[2] / voxel_size) / 2))  # half-number of pixels corresponding to the FOV
+new_shape = [max(numz, 2*z_pixel_FOV), max(numy, 2*y_pixel_FOV), max(numx, 2*x_pixel_FOV)]
 amp = pu.crop_pad(array=amp, output_shape=new_shape, debugging=False)
 numz, numy, numx = amp.shape
 print("Cropped/padded data size: (", numz, ',', numy, ',', numx, ')')
@@ -68,14 +71,22 @@ print("Cropped/padded data size: (", numz, ',', numy, ',', numx, ')')
 ###################################
 # plot 3D isosurface using mayavi #
 ###################################
-grid_qx, grid_qz, grid_qy = np.mgrid[0:numz*voxel_size:voxel_size,
-                                     0:numy*voxel_size:voxel_size,
-                                     0:numx*voxel_size:voxel_size]
+grid_qx, grid_qz, grid_qy = np.mgrid[0:2*z_pixel_FOV*voxel_size:voxel_size,
+                                     0:2*y_pixel_FOV*voxel_size:voxel_size,
+                                     0:2*x_pixel_FOV*voxel_size:voxel_size]
+extent = [0, 2*z_pixel_FOV*voxel_size, 0, 2*y_pixel_FOV*voxel_size, 0, 2*x_pixel_FOV*voxel_size]
 # in CXI convention, z is downstream, y vertical and x outboard
 
 mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0, 0, 0))
-mlab.contour3d(grid_qx, grid_qz, grid_qy, amp, contours=1, opacity=0.5, colormap="jet")
-# mlab.colorbar(orientation="vertical", nb_labels=6)
-mlab.outline(line_width=2.0)
+mlab.contour3d(grid_qx, grid_qz, grid_qy,
+               amp[numz // 2 - z_pixel_FOV:numz // 2 + z_pixel_FOV,
+                   numy // 2 - y_pixel_FOV:numy // 2 + y_pixel_FOV,
+                   numx // 2 - x_pixel_FOV:numx // 2 + x_pixel_FOV],
+               contours=[threshold_isosurface], color=(0.7, 0.7, 0.7))
+mlab.outline(extent=extent, line_width=2.0)
+ax = mlab.axes(extent=extent, line_width=2.0, nb_labels=int(1+field_of_view[0]/tick_spacing))
+ax.label_text_property.opacity = 0.0
+ax.title_text_property.opacity = 0.0
+# mlab.axes(xlabel='x', ylabel='y', zlabel='z',ranges=(1000,1100,1200),nb_labels=5)
 # mlab.axes(ranges=[-0.1, 0.05, 2.71, 2.83, 0.05, 0.19], nb_labels=3, xlabel='z', ylabel='y', zlabel='x')  #
 mlab.show()
