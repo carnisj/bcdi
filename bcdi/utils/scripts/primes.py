fced38ae76c09458e1cda3347d1b0d16ebaa0034
Jerome Carnis <jerome.carnis@desy.de>
2020-01-31 09:06:27 +0100

    Add comments

--- bcdi/utils/scripts/primes.py
+++ bcdi/utils/scripts/primes.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
-""""
-Check smaller or higher prime of a number
-"""
+
+# BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-present : DESY PHOTON SCIENCE
+#       authors:
+#         Jerome Carnis, carnis_jerome@yahoo.fr
+
 import numpy as np
-my_nb = 350
+
+helptext = """
+Check smaller or higher prime of a number, in order to determine the correct FFT window size for phase retrieval.
+Adapted from PyNX
+"""
+
+my_nb = 450
 
 
 def primes(n):
