Refactor: use correct name for average (angular instead of radial)

bcdi/utils/scripts/angular_average.py
@@ -14,7 +14,7 @@ sys.path.append('//win.desy.de/home/carnisj/My Documents/myscripts/bcdi/')
 import bcdi.graph.graph_utils as gu
 
 helptext = """
-Plot a 1D radial average of a 3D reciprocal space map, based on the position of the origin (direct beam or Bragg peak). 
+Plot a 1D angular average of a 3D reciprocal space map, based on the position of the origin (direct beam or Bragg peak). 
 
 If q values are provided, the data can be in an orthonormal frame or not (detector frame in Bragg CDI). The unit
 expected for q values is 1/nm.
@@ -24,14 +24,14 @@ If q values are not provided, the data is supposed to be in an orthonormal frame
 
 root_folder = 'D:/data/P10_August2019/data/magnetite_A2_new_00013/pynx/'
 load_qvalues = True  # True if the q values are provided
-load_mask = True  # True to load a mask, masked points are not used for radial average
+load_mask = True  # True to load a mask, masked points are not used for angular average
 origin = [297, 242, 219]  # [np.nan, np.nan, np.nan] #
-# position in pixels of the origin of the radial average in the array.
+# position in pixels of the origin of the angular average in the array.
 # if a nan value is used, the origin will be set at the middle of the array in the corresponding dimension.
 threshold = 1  # data < threshold will be set to 0
 debug = False  # True to show more plots
-xlim = [0, 1]  # limits used for the horizontal axis of the radial plot
-ylim = [0, 7]  # limits used for the vertical axis of the radial plot
+xlim = [0, 1]  # limits used for the horizontal axis of the angular plot
+ylim = [0, 7]  # limits used for the vertical axis of the angular plot
 ##########################
 # end of user parameters #
 ##########################
@@ -116,41 +116,41 @@ if debug:
 print('Distance max:', distances.max(), ' (1/nm) at voxel:', np.unravel_index(abs(distances).argmax(), distances.shape))
 print('Distance:', distances[origin[0], origin[1], origin[2]], ' (1/nm) at voxel:', origin)
 nb_bins = nz // 4
-radial_avg = np.zeros(nb_bins)
+angular_avg = np.zeros(nb_bins)
 dq = distances.max() / nb_bins  # in 1/A
 q_axis = np.linspace(0, distances.max(), endpoint=True, num=nb_bins+1)  # in pixels or 1/nm
 
 for index in range(nb_bins):
     temp = diff_pattern[np.logical_and((distances < q_axis[index+1]), (distances >= q_axis[index]))]
-    radial_avg[index] = temp[np.logical_and((~np.isnan(temp)), (temp != 0))].mean()
+    angular_avg[index] = temp[np.logical_and((~np.isnan(temp)), (temp != 0))].mean()
 q_axis = q_axis[:-1]
 
 del diff_pattern
 gc.collect()
 
-#######################
-# save radial average #
-#######################
-np.savez_compressed(root_folder + 'q+radial_avg.npz', distances=q_axis, average=radial_avg)
+########################
+# save angular average #
+########################
+np.savez_compressed(root_folder + 'q+angular_avg.npz', distances=q_axis, average=angular_avg)
 
 ################################
 # plot and save the 1D average #
 ################################
 # prepare for masking arrays - 'conventional' arrays won't do it
-y_values = np.ma.array(radial_avg)
+y_values = np.ma.array(angular_avg)
 # mask values below a certain threshold
 y_values_masked = np.ma.masked_where(np.isnan(y_values), y_values)
 
 fig, ax0 = plt.subplots(1, 1)
 plt0 = ax0.plot(q_axis, np.log10(y_values_masked), 'r')
 plt.xlabel('q (1/nm)')
-plt.ylabel('radial average (A.U.)')
+plt.ylabel('Angular average (A.U.)')
 plt.xlim(xlim[0], xlim[1])
 plt.ylim(ylim[0], ylim[1])
-plt.savefig(root_folder + 'radial_avg_labels.png')
+plt.savefig(root_folder + 'angular_avg_labels.png')
 ax0.tick_params(labelbottom=False, labelleft=False)
 plt.xlabel('')
 plt.ylabel('')
-plt.savefig(root_folder + 'radial_avg.png')
+plt.savefig(root_folder + 'angular_avg.png')
 plt.ioff()
 plt.show()