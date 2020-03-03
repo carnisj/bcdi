d3b69dece0a42c3d0dbb96c29374bb7f6a20f654
Jerome Carnis <jerome.carnis@desy.de>
2020-03-03 11:14:26 +0100

    Feature: implement a simple simulation of fcc lattice convoluted with gaussian

--- bcdi/simulation/scripts/simu_diffpattern_CDI.py
+++ bcdi/simulation/scripts/simu_diffpattern_CDI.py
@@ -7,21 +7,14 @@
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 
 import numpy as np
-from numpy.random import poisson
-from numpy.fft import fftn, fftshift
 from matplotlib import pyplot as plt
 from scipy.signal import convolve
-import tkinter as tk
-from tkinter import filedialog
-from scipy.interpolate import RegularGridInterpolator
-import gc
-import os
+from scipy.spatial.transform import Rotation
 import sys
 sys.path.append('C:/Users/Jerome/Documents/myscripts/bcdi/')
 import bcdi.graph.graph_utils as gu
 import bcdi.experiment.experiment_utils as exp
 import bcdi.postprocessing.postprocessing_utils as pu
-import bcdi.preprocessing.preprocessing_utils as pru
 import bcdi.simulation.simulation_utils as simu
 
 helptext = """
@@ -35,11 +28,11 @@ savedir = "D:/data/P10_August2019/data/gold2_2_00515/simu/"
 # sample setup #
 ################
 unitcell = 'fcc'
-unitcell_param = 23  # in nm, unit cell parameter
+unitcell_param = 22.4  # in nm, unit cell parameter
 ######################
 # sample orientation #
 ######################
-angles = [0, 0, 0]  # angle for the rotation around z, y and x respectively
+angles = [0, 30, 0]  # in degrees, rotation around z downstream, y vertical up and x outboard respectively
 #######################
 # beamline parameters #
 #######################
@@ -51,9 +44,9 @@ energy = 8700  # in ev X-ray energy
 detector = "Eiger4M"  # "Eiger2M" or "Maxipix" or "Eiger4M"
 direct_beam = (1349, 1321)  # tuple of int (vertical, horizontal): position of the direct beam in pixels
 # this parameter is important for gridding the data onto the laboratory frame
-roi_detector = [direct_beam[0] - 500, direct_beam[0] + 500, direct_beam[1] - 500, direct_beam[1] + 500]
+roi_detector = []  # [direct_beam[0] - 900, direct_beam[0] + 900, direct_beam[1] - 900, direct_beam[1] + 900]
 # [Vstart, Vstop, Hstart, Hstop]
-binning = [2, 2, 2]  # binning of the detector
+binning = [4, 4, 4]  # binning of the detector
 ###########
 # options #
 ###########
@@ -67,30 +60,63 @@ debug = True  # True to see more plots
 #######################
 detector = exp.Detector(name=detector, binning=binning, roi=roi_detector)
 
+nbz, nby, nbx = int(np.floor((detector.roi[3] - detector.roi[2]) / detector.binning[2])), \
+                   int(np.floor((detector.roi[1] - detector.roi[0]) / detector.binning[1])), \
+                   int(np.floor((detector.roi[3] - detector.roi[2]) / detector.binning[2]))
+# for P10 data the rotation is around y vertical, hence gridded data range & binning in z and x are identical
+
 ###################
 # define colormap #
 ###################
 bad_color = '1.0'  # white background
 colormap = gu.Colormap(bad_color=bad_color)
 my_cmap = colormap.cmap
+plt.ion()
 
-#########################################
-# create the lattice and the peak shape #
-#########################################
-lattice = simu.lattice(energy=energy, sdd=sdd, direct_beam=direct_beam, detector=detector, unitcell=unitcell,
-                       unitcell_param=unitcell_param)
-peak_shape = pu.gaussian_kernel(ndim=3, kernel_length=21, sigma=3, debugging=True)
+######################
+# create the lattice #
+######################
+lattice, peaks = simu.lattice(energy=energy, sdd=sdd, direct_beam=direct_beam, detector=detector, unitcell=unitcell,
+                              unitcell_param=unitcell_param)
+# peaks in the format [[h, l, k], ...]: CXI convention downstream , vertical up, outboard
+for idx in range(len(peaks)):
+    print(peaks[idx])
+struct_array = np.zeros((nbz, nby, nbx))
+for [piz, piy, pix] in lattice:
+    struct_array[piz, piy, pix] = 1
 
 ######################
 # rotate the lattice #
 ######################
+# rotation = Rotation.from_euler('zyx', angles, degrees=True)
+# struct_array = rotation.apply(struct_array)
 
 
 ##############################################
 # convolute the lattice with a 3D peak shape #
 ##############################################
-diffpattern = convolve(lattice, peak_shape, mode='same')
-gu.multislices_plot(diffpattern, sum_frames=False, title='Simulated diffraction pattern', vmin=0, plot_colorbar=True,
-                    cmap=my_cmap, is_orthogonal=True, reciprocal_space=True)
+peak_shape = pu.gaussian_kernel(ndim=3, kernel_length=21, sigma=3, debugging=False)
+diffpattern = convolve(struct_array, peak_shape, mode='same')
 
+# direct beam position after binning
+center_z = int((direct_beam[1] - detector.roi[2]) / detector.binning[2])  # horizontal downstream
+# same orientation as detector X rotated by 90 deg at P10, along z (or qx)
+directbeam_y = nby - int((direct_beam[0] - detector.roi[0]) / detector.binning[1])  # vertical
+# detector Y along vertical down, opposite to y (and qz)
+directbeam_x = nbx - int((direct_beam[1] - detector.roi[2]) / detector.binning[2])  # horizontal
+# detector X inboard, opposite to x (and qy)
+
+fig, _, _ = gu.multislices_plot(diffpattern, sum_frames=False, title='Simulated diffraction pattern', vmin=0,
+                                slice_position=[center_z, directbeam_y, directbeam_x], plot_colorbar=True,
+                                cmap=my_cmap, is_orthogonal=True, reciprocal_space=True)
+fig.text(0.60, 0.20, "Direct beam (Y,X) =" + str(directbeam_y) + "," + str(directbeam_x), size=20)
+plt.pause(0.1)
+
+fig, _, _ = gu.multislices_plot(diffpattern, sum_frames=True, title='Simulated diffraction pattern', vmin=0,
+                                plot_colorbar=True, cmap=my_cmap, is_orthogonal=True, reciprocal_space=True)
+fig.text(0.60, 0.20, "Direct beam (Y,X) =" + str(directbeam_y) + "," + str(directbeam_x), size=20)
+plt.pause(0.1)
+
+plt.ioff()
 plt.show()
+
