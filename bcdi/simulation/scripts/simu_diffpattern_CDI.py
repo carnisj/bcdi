fe9bc791a5a55beec0bfdfc9cf26ec486b403f17
Jerome Carnis <jerome.carnis@desy.de>
2020-03-03 16:44:03 +0100

    Refactor: use a faster for loop instead of convolution to put 3D gaussian at lattice points

--- bcdi/simulation/scripts/simu_diffpattern_CDI.py
+++ bcdi/simulation/scripts/simu_diffpattern_CDI.py
@@ -8,8 +8,6 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.signal import convolve
-from scipy.spatial.transform import Rotation
 import sys
 sys.path.append('C:/Users/Jerome/Documents/myscripts/bcdi/')
 import bcdi.graph.graph_utils as gu
@@ -50,6 +48,7 @@ binning = [4, 4, 4]  # binning of the detector
 ###########
 # options #
 ###########
+kernel_length = 21  # width of the 3D gaussian window
 debug = True  # True to see more plots
 ##################################
 # end of user-defined parameters #
@@ -77,27 +76,32 @@ plt.ion()
 # create the lattice #
 ######################
 lattice, peaks = simu.lattice(energy=energy, sdd=sdd, direct_beam=direct_beam, detector=detector, unitcell=unitcell,
-                              unitcell_param=unitcell_param)
+                              unitcell_param=unitcell_param, euler_angles=angles)
 # peaks in the format [[h, l, k], ...]: CXI convention downstream , vertical up, outboard
 for idx in range(len(peaks)):
-    print(peaks[idx])
+    print('Miller indices:', peaks[idx], '    at pixels:', lattice[idx])
 struct_array = np.zeros((nbz, nby, nbx))
 for [piz, piy, pix] in lattice:
     struct_array[piz, piy, pix] = 1
 
-######################
-# rotate the lattice #
-######################
-# rotation = Rotation.from_euler('zyx', angles, degrees=True)
-# struct_array = rotation.apply(struct_array)
-
-
 ##############################################
 # convolute the lattice with a 3D peak shape #
 ##############################################
-peak_shape = pu.gaussian_kernel(ndim=3, kernel_length=21, sigma=3, debugging=False)
-diffpattern = convolve(struct_array, peak_shape, mode='same')
+# since we have a small list of peaks, do not use convolution (too slow) but for loop
+peak_shape = pu.gaussian_kernel(ndim=3, kernel_length=kernel_length, sigma=3, debugging=False)
 
+for [piz, piy, pix] in lattice:
+    startz = max(0, int(piz-kernel_length//2))
+    stopz = min(nbz-1, int(piz+kernel_length//2))
+    starty = max(0, int(piy-kernel_length//2))
+    stopy = min(nby-1, int(piy+kernel_length//2))
+    startx = max(0, int(pix-kernel_length//2))
+    stopx = min(nbx-1, int(pix+kernel_length//2))
+    struct_array[startz:stopz+1, starty:stopy+1, startx:stopx+1] = peak_shape
+
+###############
+# plot result #
+###############
 # direct beam position after binning
 center_z = int((direct_beam[1] - detector.roi[2]) / detector.binning[2])  # horizontal downstream
 # same orientation as detector X rotated by 90 deg at P10, along z (or qx)
@@ -106,13 +110,13 @@ directbeam_y = nby - int((direct_beam[0] - detector.roi[0]) / detector.binning[1
 directbeam_x = nbx - int((direct_beam[1] - detector.roi[2]) / detector.binning[2])  # horizontal
 # detector X inboard, opposite to x (and qy)
 
-fig, _, _ = gu.multislices_plot(diffpattern, sum_frames=False, title='Simulated diffraction pattern', vmin=0,
+fig, _, _ = gu.multislices_plot(struct_array, sum_frames=False, title='Simulated diffraction pattern', vmin=0,
                                 slice_position=[center_z, directbeam_y, directbeam_x], plot_colorbar=True,
                                 cmap=my_cmap, is_orthogonal=True, reciprocal_space=True)
 fig.text(0.60, 0.20, "Direct beam (Y,X) =" + str(directbeam_y) + "," + str(directbeam_x), size=20)
 plt.pause(0.1)
 
-fig, _, _ = gu.multislices_plot(diffpattern, sum_frames=True, title='Simulated diffraction pattern', vmin=0,
+fig, _, _ = gu.multislices_plot(struct_array, sum_frames=True, title='Simulated diffraction pattern', vmin=0,
                                 plot_colorbar=True, cmap=my_cmap, is_orthogonal=True, reciprocal_space=True)
 fig.text(0.60, 0.20, "Direct beam (Y,X) =" + str(directbeam_y) + "," + str(directbeam_x), size=20)
 plt.pause(0.1)
