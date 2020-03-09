af74d34aa05a3e3f6d60cf6b9effa16fe4616e02
Jerome Carnis <jerome.carnis@desy.de>
2020-03-09 14:22:41 +0100

    Refactor: change optional plots

--- bcdi/simulation/scripts/simu_diffpattern_CDI.py
+++ bcdi/simulation/scripts/simu_diffpattern_CDI.py
@@ -30,7 +30,7 @@ unitcell_param = 22.4  # in nm, unit cell parameter
 ######################
 # sample orientation #
 ######################
-angles = [-5, 15, -5]  # in degrees, rotation around qx downstream, qz vertical up and qy outboard respectively
+angles = [0, 26, 0]  # in degrees, rotation around qx downstream, qz vertical up and qy outboard respectively
 #######################
 # beamline parameters #
 #######################
@@ -115,34 +115,35 @@ if debug:
                 str(binning[1]) + '_' + str(binning[2]) + '_rot_' + str(angles[0]) + '_' + str(angles[1]) + '_' +
                 str(angles[2]) + '.png')
 
-    fig, _, _ = gu.multislices_plot(struct_array, sum_frames=True, title='Simulated diffraction pattern', vmin=0,
-                                    vmax=maxpeak, plot_colorbar=True, cmap=my_cmap, is_orthogonal=True,
-                                    reciprocal_space=True)
-    fig.text(0.60, 0.30, "Origin of reciprocal space  (Qx,Qz,Qy) = " + str(pivot[0]) + "," + str(pivot[1]) +
-             "," + str(pivot[2]), size=12)
-    fig.text(0.60, 0.25, "Energy = " + str(energy/1000) + " keV", size=12)
-    fig.text(0.60, 0.20, "SDD = " + str(sdd) + " m", size=12)
-    fig.text(0.60, 0.15, unitcell + " unit cell of parameter = " + str(unitcell_param) + " nm", size=12)
-    fig.text(0.60, 0.10, "Rotation of the unit cell in degrees (Qx, Qz, Qy) = " + str(angles[0]) + "," + str(angles[1])
-             + "," + str(angles[2]), size=12)
-    plt.pause(0.1)
-    plt.savefig(savedir + 'sum_' + str(nbz) + '_' + str(nby) + '_' + str(nbx) + '_' + str(binning[0]) + '_' +
-                str(binning[1]) + '_' + str(binning[2]) + '_rot_' + str(angles[0]) + '_' + str(angles[1]) + '_' +
-                str(angles[2]) + '.png')
-
-fig, _, _ = gu.contour_slices(struct_array, (qx, qz, qy), sum_frames=False, title='Simulated diffraction pattern',
-                              slice_position=[pivot[0], pivot[1], pivot[2]],
-                              levels=np.linspace(0, struct_array.max(), 10, endpoint=False),
-                              plot_colorbar=False, scale='linear', is_orthogonal=True, reciprocal_space=True)
+fig, _, _ = gu.multislices_plot(struct_array, sum_frames=True, title='Simulated diffraction pattern', vmin=0,
+                                vmax=maxpeak, plot_colorbar=True, cmap=my_cmap, is_orthogonal=True,
+                                reciprocal_space=True)
+fig.text(0.60, 0.30, "Origin of reciprocal space  (Qx,Qz,Qy) = " + str(pivot[0]) + "," + str(pivot[1]) +
+         "," + str(pivot[2]), size=12)
 fig.text(0.60, 0.25, "Energy = " + str(energy/1000) + " keV", size=12)
 fig.text(0.60, 0.20, "SDD = " + str(sdd) + " m", size=12)
 fig.text(0.60, 0.15, unitcell + " unit cell of parameter = " + str(unitcell_param) + " nm", size=12)
 fig.text(0.60, 0.10, "Rotation of the unit cell in degrees (Qx, Qz, Qy) = " + str(angles[0]) + "," + str(angles[1])
          + "," + str(angles[2]), size=12)
 plt.pause(0.1)
-plt.savefig(savedir + 'q_central_slice_' + str(nbz) + '_' + str(nby) + '_' + str(nbx) + '_' + str(binning[0]) + '_' +
+plt.savefig(savedir + 'sum_' + str(nbz) + '_' + str(nby) + '_' + str(nbx) + '_' + str(binning[0]) + '_' +
             str(binning[1]) + '_' + str(binning[2]) + '_rot_' + str(angles[0]) + '_' + str(angles[1]) + '_' +
             str(angles[2]) + '.png')
+
+if debug:
+    fig, _, _ = gu.contour_slices(struct_array, (qx, qz, qy), sum_frames=False, title='Simulated diffraction pattern',
+                                  slice_position=[pivot[0], pivot[1], pivot[2]],
+                                  levels=np.linspace(0, struct_array.max(), 10, endpoint=False),
+                                  plot_colorbar=False, scale='linear', is_orthogonal=True, reciprocal_space=True)
+    fig.text(0.60, 0.25, "Energy = " + str(energy/1000) + " keV", size=12)
+    fig.text(0.60, 0.20, "SDD = " + str(sdd) + " m", size=12)
+    fig.text(0.60, 0.15, unitcell + " unit cell of parameter = " + str(unitcell_param) + " nm", size=12)
+    fig.text(0.60, 0.10, "Rotation of the unit cell in degrees (Qx, Qz, Qy) = " + str(angles[0]) + "," + str(angles[1])
+             + "," + str(angles[2]), size=12)
+    plt.pause(0.1)
+    plt.savefig(savedir + 'q_central_slice_' + str(nbz) + '_' + str(nby) + '_' + str(nbx) + '_' + str(binning[0]) + '_' +
+                str(binning[1]) + '_' + str(binning[2]) + '_rot_' + str(angles[0]) + '_' + str(angles[1]) + '_' +
+                str(angles[2]) + '.png')
 
 fig, _, _ = gu.contour_slices(struct_array, (qx, qz, qy), sum_frames=True, title='Simulated diffraction pattern',
                               levels=np.linspace(0, struct_array.max(), 10, endpoint=False),
