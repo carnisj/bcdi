21404521640010d0b0513da75518a37c35787ccb
Jerome Carnis <jerome.carnis@desy.de>
2020-04-22 16:57:19 +0200

    Feature: implement angular_avg() for the angular averaging of a 3D dataset

--- bcdi/utils/utilities.py
+++ bcdi/utils/utilities.py
@@ -10,6 +10,78 @@ import os
 import h5py
 import numpy as np
 from scipy.interpolate import interp1d
+import sys
+sys.path.append('C:/Users/Jerome/Documents/myscripts/bcdi/')
+import bcdi.graph.graph_utils as gu
+
+
+def angular_avg(data, q_values, mask=None, origin=None, debugging=False):
+    """
+    Calculate an angular average of a 3D reciprocal space dataset given q values and the position of the origin of the
+    reciprocal space.
+
+    :param data: 3D reciprocal space data gridded in the orthonormal frame (qx downstream, qz vertical up, qy outboard)
+    :param q_values: tuple of 3 1-D arrays: (qx downstream, qz vertical up, qy outboard)
+    :param mask: 3D array of the same shape as data. 1 for a masked voxel, 0 otherwise
+    :param origin: position in pixels of the origin of the reciprocal space
+    :param debugging: True to see plots
+    :return: q_axis, angular mean average, angular median average
+    """
+    if len(q_values) != 3:
+        raise ValueError("q_values should be a tuple of three 1D arrays")
+    if data.ndim != 3:
+        raise ValueError("data should be a 3D array")
+    if mask is None:
+        mask = np.zeros(data.shape)
+    else:
+        assert mask.shape == data.shape, "mask should have the same shape as data"
+    qx, qz, qy = q_values
+    nz, ny, nx = data.shape
+
+    if len(qx) != nz or len(qz) != ny or len(qy) != nx:
+        raise ValueError("size of q values incompatible with data shape")
+
+    if origin is None:
+        origin = (int(nz // 2), int(ny // 2), int(nx // 2))
+    elif len(origin) != 3:
+        raise ValueError("origin should be a tuple of 3 elements")
+
+    # calculate ditance matrix
+    distances = np.sqrt((qx[:, np.newaxis, np.newaxis] - qx[origin[0]]) ** 2 +
+                        (qz[np.newaxis, :, np.newaxis] - qz[origin[1]]) ** 2 +
+                        (qy[np.newaxis, np.newaxis, :] - qy[origin[2]]) ** 2)
+    if debugging:
+        gu.multislices_plot(distances, sum_frames=False, plot_colorbar=True, title='distances_q', scale='linear',
+                            vmin=np.nan, vmax=np.nan, reciprocal_space=True, is_orthogonal=True)
+
+    # average over spherical shells
+    print('Distance max:', distances.max(), ' (1/nm) at voxel:',
+          np.unravel_index(abs(distances).argmax(), distances.shape))
+    print('Distance:', distances[origin[0], origin[1], origin[2]], ' (1/nm) at voxel:',
+          origin)
+    nb_bins = nz // 4
+    ang_avg = np.zeros(nb_bins)  # angular average using the mean value
+    ang_median = np.zeros(nb_bins)  # angular average using the median value
+    q_axis = np.linspace(0, distances.max(), endpoint=True, num=nb_bins + 1)  # in pixels or 1/nm
+
+    for index in range(nb_bins):
+        indices = np.logical_and((distances < q_axis[index + 1]), (distances >= q_axis[index]))
+        temp_data = data[indices]
+        temp_mask = mask[indices]
+
+        ang_avg[index] = temp_data[np.logical_and((~np.isnan(temp_data)), (temp_mask != 1))].mean()
+        ang_median[index] = np.median(temp_data[np.logical_and((~np.isnan(temp_data)), (temp_mask != 1))])
+
+    q_axis = q_axis[:-1]
+
+    # prepare for masking arrays - 'conventional' arrays won't do it
+    y_mean = np.ma.array(ang_avg)
+    y_median = np.ma.array(ang_median)
+    # mask nan values
+    y_mean_masked = np.ma.masked_where(np.isnan(y_mean), y_mean)
+    y_median_masked = np.ma.masked_where(np.isnan(y_median), y_median)
+
+    return q_axis, y_mean_masked, y_median_masked
 
 
 def find_nearest(original_array, array_values):
