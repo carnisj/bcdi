29a7a1358e27891800fc96ffcbf6f4b09299369f
Jerome Carnis <jerome.carnis@desy.de>
2020-05-30 08:32:20 +0200

    Refactor: add a ROI counter to see the evolution of the integrated intensity

--- bcdi/preprocessing/scripts/read_data_P10.py
+++ bcdi/preprocessing/scripts/read_data_P10.py
@@ -23,16 +23,18 @@ import bcdi.graph.graph_utils as gu
 import bcdi.experiment.experiment_utils as exp
 import bcdi.preprocessing.preprocessing_utils as pru
 
-scan = 0  # scan number as it appears in the folder name
+scan = 22  # scan number as it appears in the folder name
 sample_name = "gold_2_2_2"  # without _ at the end
 rootdir = "D:/data/P10_August2019/data/"
-image_nb = 1  # np.arange(1, 381+1)
+image_nb = np.arange(1, 381+1)
 # list of file numbers, e.g. [1] for gold_2_2_2_00022_data_000001.h5
 detector = "Eiger4M"    # "Eiger2M" or "Maxipix" or "Eiger4M"
+counter_roi = []  # plot the integrated intensity in this region of interest. Leave it to [] to use the full detector
+# [Vstart, Vstop, Hstart, Hstop]
 high_threshold = 9  # data points where log10(data) > high_threshold will be masked
 # if data is a series, the condition becomes log10(data.sum(axis=0)) > high_threshold
 savedir = ''  # images will be saved here, leave it to '' otherwise (default to data directory's parent)
-is_scan = False  # set to True is the measurement is a scan or a time series, False for a single image
+is_scan = True  # set to True is the measurement is a scan or a time series, False for a single image
 compare_ends = True  # set to True to plot the difference between the last frame and the first frame
 save_mask = False  # True to save the mask as 'hotpixels.npz'
 ##########################
@@ -43,13 +45,20 @@ save_mask = False  # True to save the mask as 'hotpixels.npz'
 # Initialize detector #
 #######################
 detector = exp.Detector(name=detector)
+nb_pixel_y, nb_pixel_x = detector.nb_pixel_y, detector.nb_pixel_x
 
 ####################
 # Initialize paths #
 ####################
 if type(image_nb) == int:
     image_nb = [image_nb]
+if len(counter_roi) == 0:
+    counter_roi = [0, nb_pixel_y, 0, nb_pixel_x]
 
+assert (counter_roi[0] >= 0
+        and counter_roi[1] <= nb_pixel_y
+        and counter_roi[2] >= 0
+        and counter_roi[3] <= nb_pixel_x), 'counter_roi setting does not match the detector size'
 nb_files = len(image_nb)
 
 if is_scan:  # scan or time series
@@ -64,9 +73,10 @@ if savedir == '':
 #############
 # Load data #
 #############
-sumdata = np.zeros((detector.nb_pixel_y, detector.nb_pixel_x))
-mask = np.zeros((detector.nb_pixel_y, detector.nb_pixel_x))
-
+plt.ion()
+sumdata = np.zeros((nb_pixel_y, nb_pixel_x))
+mask = np.zeros((nb_pixel_y, nb_pixel_x))
+counter = []
 for idx in range(nb_files):
     if is_scan:
         ccdfiletmp = os.path.join(datadir, sample_name + '_' + str('{:05d}'.format(scan)) +
@@ -78,7 +88,8 @@ for idx in range(nb_files):
     h5file = h5py.File(ccdfiletmp, 'r')
     data = h5file['entry']['data']['data'][:]
     nbz, nby, nbx = data.shape
-
+    [counter.append(data[index, counter_roi[0]:counter_roi[1], counter_roi[2]:counter_roi[3]].sum())
+     for index in range(nbz)]
     if compare_ends and nb_files == 1:
         data_start, _ = pru.mask_eiger4m(data=data[0, :, :], mask=mask)
         data_start[np.log10(data_start) > high_threshold] = 0
@@ -116,6 +127,12 @@ if save_mask:
 
 y0, x0 = np.unravel_index(abs(sumdata).argmax(), sumdata.shape)
 print("Max at (y, x): ", y0, x0, ' Max = ', int(sumdata[y0, x0]))
+
+if len(counter) > 1:
+    plt.figure()
+    plt.plot(np.asarray(counter))
+    plt.title('Integrated intensity in counter_roi')
+    plt.pause(0.1)
 
 fig, _, _ = gu.imshow_plot(sumdata, plot_colorbar=True, title=plot_title, vmin=0, scale='log')
 np.savez_compressed(savedir + 'hotpixels.npz', mask=mask)
