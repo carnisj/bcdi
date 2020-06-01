a9cf1bfa0a10bd2c7e9f4b96aeca6d67931771da
Jerome Carnis <jerome.carnis@desy.de>
2020-06-01 22:01:46 +0200

    Refactor: use module functions instead of inline script

--- bcdi/postprocessing/scripts/extract_bulk_surface.py
+++ bcdi/postprocessing/scripts/extract_bulk_surface.py
@@ -2,83 +2,48 @@
 
 # BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
 #   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-present : DESY PHOTON SCIENCE
 #       authors:
-#         Jerome Carnis, jerome.carnis@esrf.fr
+#         Jerome Carnis, carnis_jerome@yahoo.fr
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
 import tkinter as tk
 from tkinter import filedialog
-from scipy.signal import convolve
 import os
-import logging
+import sys
+sys.path.append('D:/myscripts/bcdi/')
+import bcdi.graph.graph_utils as gu
+import bcdi.postprocessing.postprocessing_utils as pu
 
 helptext = """
 # TODO refactor
 """
 
-scan = 2191  # spec scan number
-datadir = "C:/Users/carnis/Work Folders/Documents/data/CH4760_Pt/S"+str(scan)+"/pynxraw/"
-support_threshold = 0.25  # threshold for support determination
-save = 1  # 1 to save
-debug = 1  # 1 to see all plots
-############################################################
-logger = logging.getLogger()
-# define a colormap
-cdict = {'red':  ((0.0, 1.0, 1.0),
-                  (0.11, 0.0, 0.0),
-                  (0.36, 0.0, 0.0),
-                  (0.62, 1.0, 1.0),
-                  (0.87, 1.0, 1.0),
-                  (1.0, 0.0, 0.0)),
-         'green': ((0.0, 1.0, 1.0),
-                   (0.11, 0.0, 0.0),
-                   (0.36, 1.0, 1.0),
-                   (0.62, 1.0, 1.0),
-                   (0.87, 0.0, 0.0),
-                   (1.0, 0.0, 0.0)),
-         'blue': ((0.0, 1.0, 1.0),
-                  (0.11, 1.0, 1.0),
-                  (0.36, 1.0, 1.0),
-                  (0.62, 0.0, 0.0),
-                  (0.87, 0.0, 0.0),
-                  (1.0, 0.0, 0.0))}
-my_cmap = LinearSegmentedColormap('my_colormap', cdict, 256)
+scan = 1301  # spec scan number
+root_folder = "D:/data/SIXS_2019_Ni/"
+sample_name = "S"  # "S"
+datadir = root_folder + sample_name + str(scan) + "/pynxraw/"
+support_threshold = 0.30  # threshold applied to the modulus for determining the support
+normalize = False  # if True, will normalize the histograms to the respective number of points
+plot_scale = 'linear'  # 'log' or 'linear', Y scale for the histograms
+xlim = [-0.002, 0.002]  # limits used for the horizontal axis of histograms, leave None otherwise
+ylim = None  # limits used for the vertical axis of histograms, leave None otherwise
+save_txt = False  # True to save the strain values for the surface, the bulk and the full support in txt files
+debug = True  # True to see more plots
+##########################
+# end of user parameters #
+##########################
 
+###################
+# define colormap #
+###################
+colormap = gu.Colormap()
+my_cmap = colormap.cmap
 
-def calc_coordination(myamp, mythreshold, debugging=0):
-    nbz, nby, nbx = myamp.shape
-    mysupport = np.zeros((nbz, nby, nbx))
-    mysupport[myamp > mythreshold * abs(myamp).max()] = 1
-
-    mykernel = np.ones((3, 3, 3))
-    mycoord = np.rint(convolve(mysupport, mykernel, mode='same'))
-    mycoord = mycoord.astype(int)
-
-    if debugging == 1:
-        plt.figure(figsize=(18, 15))
-        plt.subplot(2, 2, 1)
-        plt.imshow(mycoord[:, :, nbx // 2])
-        plt.colorbar()
-        plt.axis('scaled')
-        plt.title("Coordination matrix in middle slice in YZ")
-        plt.subplot(2, 2, 2)
-        plt.imshow(mycoord[:, nby // 2, :])
-        plt.colorbar()
-        plt.title("Coordination matrix in middle slice in XZ")
-        plt.axis('scaled')
-        plt.subplot(2, 2, 3)
-        plt.imshow(mycoord[nbz // 2, :, :])
-        plt.gca().invert_yaxis()
-        plt.colorbar()
-        plt.title("Coordination matrix in middle slice in XY")
-        plt.axis('scaled')
-        plt.pause(0.1)
-    return mycoord
-
-
-###################################################################
+#############
+# load data #
+#############
 plt.ion()
 root = tk.Tk()
 root.withdraw()
@@ -86,72 +51,29 @@ file_path = filedialog.askopenfilename(initialdir=datadir, filetypes=[("NPZ", "*
 npzfile = np.load(file_path)
 amp = npzfile['amp']
 strain = npzfile['strain']
+nbz, nby, nbx = amp.shape
+
+######################################
+# define the support, surface & bulk #
+######################################
 support = np.zeros(amp.shape)
 support[amp > support_threshold*amp.max()] = 1
-nz, ny, nx = amp.shape
-print("Initial data size: (", nz, ',', ny, ',', nx, ')')
-coordination_matrix = calc_coordination(amp, support_threshold, debugging=0)
+coordination_matrix = pu.calc_coordination(support=support, kernel=np.ones((3, 3, 3)), debugging=debug)
 surface = np.copy(support)
 surface[coordination_matrix > 22] = 0  # remove the bulk 22
-# surface[np.nonzero(surface)] = 1
 bulk = support - surface
-bulk[np.nonzero(bulk)] = 1
-if debug == 1:
-    plt.figure(figsize=(18, 15))
-    plt.subplot(3, 3, 1)
-    plt.imshow(coordination_matrix[:, :, nx // 2], cmap=my_cmap)
-    plt.colorbar()
-    plt.axis('scaled')
-    plt.title("Coordination matrix in middle slice in YZ")
-    plt.subplot(3, 3, 2)
-    plt.imshow(coordination_matrix[:, ny // 2, :], cmap=my_cmap)
-    plt.colorbar()
-    plt.title("Coordination matrix in middle slice in XZ")
-    plt.axis('scaled')
-    plt.text(100, -75, "threshold="+str(support_threshold))
-    plt.subplot(3, 3, 3)
-    plt.imshow(coordination_matrix[nz // 2, :, :], cmap=my_cmap)
-    plt.gca().invert_yaxis()
-    plt.colorbar()
-    plt.title("Coordination matrix in middle slice in XY")
-    plt.axis('scaled')
+if debug:
+    gu.multislices_plot(surface, sum_frames=False, plot_colorbar=True, cmap=my_cmap,
+                        title='Surface layer', scale='linear', is_orthogonal=True,
+                        reciprocal_space=False)
+    gu.multislices_plot(bulk, sum_frames=False, plot_colorbar=True, cmap=my_cmap,
+                        title='Bulk', scale='linear', is_orthogonal=True,
+                        reciprocal_space=False)
 
-    plt.subplot(3, 3, 4)
-    plt.imshow(surface[:, :, nx // 2])
-    plt.colorbar()
-    plt.axis('scaled')
-    plt.title("Surface matrix in middle slice in YZ")
-    plt.subplot(3, 3, 5)
-    plt.imshow(surface[:, ny // 2, :])
-    plt.colorbar()
-    plt.title("Surface matrix in middle slice in XZ")
-    plt.axis('scaled')
-    plt.subplot(3, 3, 6)
-    plt.imshow(surface[nz // 2, :, :])
-    plt.gca().invert_yaxis()
-    plt.colorbar()
-    plt.title("Surface matrix in middle slice in XY")
-    plt.axis('scaled')
-
-    plt.subplot(3, 3, 7)
-    plt.imshow(bulk[:, :, nx // 2])
-    plt.colorbar()
-    plt.axis('scaled')
-    plt.title("Bulk matrix in middle slice in YZ")
-    plt.subplot(3, 3, 8)
-    plt.imshow(bulk[:, ny // 2, :])
-    plt.colorbar()
-    plt.title("Bulk matrix in middle slice in XZ")
-    plt.axis('scaled')
-    plt.subplot(3, 3, 9)
-    plt.imshow(bulk[nz // 2, :, :])
-    plt.gca().invert_yaxis()
-    plt.colorbar()
-    plt.title("Bulk matrix in middle slice in XY")
-    plt.axis('scaled')
-    plt.pause(0.1)
-
-if save == 1:
+##########################
+# save the strain values #
+##########################
+if save_txt:
     file_surface = open(os.path.join(datadir, "S" + str(scan) +
                                      "_threshold" + str(support_threshold) + "_surface.dat"), "w")
     file_bulk = open(os.path.join(datadir, "S" + str(scan) +
@@ -194,41 +116,68 @@ if save == 1:
     file_bulk.close()
     file_total.close()
 
-# plot histogram for surface
+###################################################
+# plot the strain histogram for the surface layer #
+###################################################
 nb_surface = len(np.nonzero(surface)[0])
-print("Surface points = ", str(nb_surface))
-hist, bin_edges = np.histogram(strain[np.nonzero(surface)], bins=50)
-hist = hist / nb_surface  # normalize the histogram to the number of points
+print("Number of surface points = ", str(nb_surface))
+print('Min surface strain = {:.5f}'.format(strain[np.nonzero(surface)].min()))
+print('Max surface strain = {:.5f}'.format(strain[np.nonzero(surface)].max()))
+hist, bin_edges = np.histogram(strain[np.nonzero(surface)], bins=100)
+hist = hist.astype(float)
+if normalize:
+    hist = hist / nb_surface  # normalize the histogram to the number of points
 hist[hist == 0] = np.nan
 x_axis = bin_edges[:-1] + (bin_edges[1] - bin_edges[0]) / 2
-fig = plt.figure()
-# plt.bar(x_axis, np.log10(hist), width=(bin_edges[1] - bin_edges[0]))
-plt.plot(x_axis, np.log10(hist))
-plt.xlim(-0.007, 0.007)
-# plt.xlim(min(x_axis), max(x_axis))
-plt.ylim(-6, 0)
-plt.title('Histogram of the strain for surface points, ' + str(nb_surface)
-          + ' points\n' + "threshold="+str(support_threshold))
+plt.figure()
+if plot_scale == 'log':
+    plt.plot(x_axis, np.log10(hist))
+else:
+    plt.plot(x_axis, hist)
+if xlim is None:
+    plt.xlim(-max(abs(x_axis)), max(abs(x_axis)))
+else:
+    assert len(xlim) == 2, 'xlim=[min, max] expected'
+    plt.xlim(xlim[0], xlim[1])
+if ylim is not None:
+    assert len(ylim) == 2, 'ylim=[min, max] expected'
+    plt.ylim(ylim[0], ylim[1])
+
+plt.title('Histogram of the strain for {:d} surface points'.format(nb_surface)
+          + "\nModulus threshold="+str(support_threshold))
 plt.pause(0.1)
 
-# plot histogram of bulk points
+##########################################
+# plot the strain histogram for the bulk #
+##########################################
 nb_bulk = len(np.nonzero(bulk)[0])
-print("Bulk points = ", str(nb_bulk))
-hist, bin_edges = np.histogram(strain[np.nonzero(bulk)], bins=50)
-hist = hist / nb_bulk  # normalize the histogram to the number of points
+print("Number of bulk points = ", str(nb_bulk))
+print('Min bulk strain = {:.5f}'.format(strain[np.nonzero(bulk)].min()))
+print('Max bulk strain = {:.5f}'.format(strain[np.nonzero(bulk)].max()))
+hist, bin_edges = np.histogram(strain[np.nonzero(bulk)], bins=100)
+hist = hist.astype(float)
+if normalize:
+    hist = hist / nb_bulk  # normalize the histogram to the number of points
 hist[hist == 0] = np.nan
 x_axis = bin_edges[:-1] + (bin_edges[1] - bin_edges[0]) / 2
-fig = plt.figure()
-# plt.bar(x_axis, np.log10(hist), width=(bin_edges[1] - bin_edges[0]))
-plt.plot(x_axis, np.log10(hist))
-plt.xlim(-0.007, 0.007)
-# plt.xlim(min(x_axis), max(x_axis))
-plt.ylim(-6, 0)
-plt.title('Histogram of the strain for bulk points, ' + str(nb_bulk)
-          + ' points\n' + "threshold="+str(support_threshold))
+plt.figure()
+if plot_scale == 'log':
+    plt.plot(x_axis, np.log10(hist))
+else:
+    plt.plot(x_axis, hist)
+if xlim is None:
+    plt.xlim(-max(abs(x_axis)), max(abs(x_axis)))
+else:
+    assert len(xlim) == 2, 'xlim=[min, max] expected'
+    plt.xlim(xlim[0], xlim[1])
+if ylim is not None:
+    assert len(ylim) == 2, 'ylim=[min, max] expected'
+    plt.ylim(ylim[0], ylim[1])
+plt.title('Histogram of the strain for {:d} bulk points'.format(nb_bulk)
+          + "\nModulus threshold="+str(support_threshold))
 plt.pause(0.1)
 
 nb_total = len(np.nonzero(support)[0])
-print("Total points = ", str(nb_total), ", surface+bulk = ", str(nb_surface+nb_bulk))
+print("Sanity check: Total points = {:d}".format(nb_total), ", surface+bulk = {:d}".format(nb_surface+nb_bulk))
 plt.ioff()
 plt.show()
