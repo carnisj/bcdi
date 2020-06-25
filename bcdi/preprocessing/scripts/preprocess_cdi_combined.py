5d8e50039a03022e178b637a3d68ab1a4dae1277
Jerome Carnis <jerome.carnis@desy.de>
2020-06-25 13:51:36 +0200

    Refactor: modify the arrangement subplots in normalization plot

--- bcdi/preprocessing/scripts/preprocess_cdi_combined.py
+++ bcdi/preprocessing/scripts/preprocess_cdi_combined.py
@@ -542,9 +542,9 @@ for scan_nb in range(len(scans)):
                                         tuple_sum_axis=(0, 1), tuple_width_v=None,
                                         tuple_width_h=None, tuple_colorbar=(False, False),
                                         tuple_vmin=(np.nan, 0), tuple_vmax=(np.nan, np.nan),
-                                        tuple_title=('monitor.min() / monitor', 'Gridded normed data\nthres. 5'),
+                                        tuple_title=('monitor.min() / monitor', 'Gridded normed data (threshold 5)\n'),
                                         tuple_scale=('linear', 'log'), xlabel=('Frame number', "Q$_y$"),
-                                        ylabel=('Counts (a.u.)', "Q$_x$"), position=(311, 212),
+                                        ylabel=('Counts (a.u.)', "Q$_x$"), position=(323, 122),
                                         is_orthogonal=not use_rawdata, reciprocal_space=True)
 
                 fig.savefig(savedir + 'monitor_gridded_S' + str(scans[scan_nb]) + '_' + str(nz) + '_' + str(ny) + '_' +
