50906957e4c5c908be5cfa70f490b1d6f5e5c910
Jerome Carnis <jerome.carnis@desy.de>
2020-07-07 15:10:57 +0200

    Refactor: modify comment

--- bcdi/preprocessing/preprocessing_utils.py
+++ bcdi/preprocessing/preprocessing_utils.py
@@ -1926,7 +1926,7 @@ def load_data(logfile, scan_number, detector, setup, flatfield=None, hotpixels=N
     if setup.beamline != 'P10':
         bin_during_loading = False
 
-    print('Detector size defined by the ROI (VxH):',
+    print('Detector ROI loaded (VxH):',
           detector.roi[1] - detector.roi[0], detector.roi[3] - detector.roi[2])
     print('Detector physical size without binning (VxH):', detector.nb_pixel_y, detector.nb_pixel_x)
     print('Detector size with binning (VxH):',
