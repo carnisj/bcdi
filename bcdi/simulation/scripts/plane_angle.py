41c4e425d11b83e387643ba97e29019e11e7c654
Jerome Carnis <jerome.carnis@desy.de>
2020-07-15 17:33:37 +0200

    Feature: calculate the basis for the most general triclinic case

--- bcdi/simulation/scripts/plane_angle.py
+++ bcdi/simulation/scripts/plane_angle.py
@@ -10,13 +10,36 @@ sys.path.append('D:/myscripts/bcdi/')
 import bcdi.simulation.simulation_utils as simu
 
 helptext = """
-Calculate the angle between to crystallographic planes expressed in the crystal basis.
+Calculate the angle between to crystallographic planes expressed in the triclinic crystal system.
 """
 
-reference_plane = [0, 1, 0]  # [0.37182, 0.78376, -0.49747]  # [0.40975, 0.29201, -0.86420]
-second_plane = [1, 1, 0]  # [-0.22923, 0.76727, -0.59896]  # [-0.19695, 0.27933, -0.93978]
-basis = (np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1]))  # components of the basis vectors in the
-# orthonormal basis ((1, 0, 0), (0, 1, 0), (0, 0, 1))
+reference_plane = [1, 0, 0]  # in the basis (b1, b2, b3)
+test_plane = [1, 0, 1]  # in the basis (b1, b2, b3)
 
-angle = simu.angle_vectors(ref_vector=reference_plane, test_vector=second_plane, basis_vectors=basis)
+alpha = 76  # in degrees, angle between b2 and b3
+beta = 78  # in degrees, angle between b1 and b3
+gamma = 87  # in degrees, angle between b1 and b2
+b1 = 0.103  # length of b1
+b2 = 0.103  # length of b2
+b3 = 0.108  # length of b3
+##################################
+# end of user-defined parameters #
+##################################
+
+####################################################################################################
+# calculate the basis vector components in the orthonormal basis [[1, 0, 0], [0, 1, 0], [0, 0, 1]] #
+####################################################################################################
+first_vector = b1 * np.array([1, 0, 0])  # the convention here is to align b1 along x
+second_vector = b2 * np.cos(gamma*np.pi/180) * np.array([1, 0, 0]) + b2 * np.sin(gamma*np.pi/180) * np.array([0, 1, 0])
+# b2 is in the plane defined by the vectors [1, 0, 0] and [0, 1, 0]
+cx = b3 * np.cos(beta*np.pi/180)
+cy = b3 * (np.cos(alpha*np.pi/180) - np.cos(beta*np.pi/180)*np.cos(gamma*np.pi/180))/np.sin(gamma*np.pi/180)
+cz = np.sqrt(b3**2 - cx**2 - cy**2)
+third_vector = cx * np.array([1, 0, 0]) + cy * np.array([0, 1, 0]) + cz * np.array([0, 0, 1])
+basis = (first_vector, second_vector, third_vector)
+print('Volume of the unit cell: {:.6f}'.format(b1*b2*cz*np.sin(gamma*np.pi/180)))
+##############################################################
+# calculate the angle between reference_plane and test_plane #
+##############################################################
+angle = simu.angle_vectors(ref_vector=reference_plane, test_vector=test_plane, basis_vectors=basis)
 print('angle = {:.2f} deg'.format(angle))
