40a02e481875476c175bc45af7976dc06d3ce2b3
Jerome Carnis <jerome.carnis@desy.de>
2020-07-16 09:57:50 +0200

    Refactor: use module functions instead of inline script

--- bcdi/simulation/scripts/plane_angle.py
+++ bcdi/simulation/scripts/plane_angle.py
@@ -15,7 +15,9 @@ Calculate the angle between to crystallographic planes expressed in the triclini
 
 reference_plane = [1, 0, 0]  # in the basis (b1, b2, b3)
 test_plane = [1, 0, 1]  # in the basis (b1, b2, b3)
-
+#################################
+# define the reciprocal lattice #
+#################################
 alpha = 76  # in degrees, angle between b2 and b3
 beta = 78  # in degrees, angle between b1 and b3
 gamma = 87  # in degrees, angle between b1 and b2
@@ -29,15 +31,9 @@ b3 = 0.108  # length of b3
 ####################################################################################################
 # calculate the basis vector components in the orthonormal basis [[1, 0, 0], [0, 1, 0], [0, 0, 1]] #
 ####################################################################################################
-first_vector = b1 * np.array([1, 0, 0])  # the convention here is to align b1 along [1, 0, 0]
-second_vector = b2 * np.cos(gamma*np.pi/180) * np.array([1, 0, 0]) + b2 * np.sin(gamma*np.pi/180) * np.array([0, 1, 0])
-# b2 is in the plane defined by the vectors [1, 0, 0] and [0, 1, 0]
-cx = b3 * np.cos(beta*np.pi/180)
-cy = b3 * (np.cos(alpha*np.pi/180) - np.cos(beta*np.pi/180)*np.cos(gamma*np.pi/180))/np.sin(gamma*np.pi/180)
-cz = np.sqrt(b3**2 - cx**2 - cy**2)
-third_vector = cx * np.array([1, 0, 0]) + cy * np.array([0, 1, 0]) + cz * np.array([0, 0, 1])
-basis = (first_vector, second_vector, third_vector)
-print('Volume of the unit cell: {:.6f}'.format(b1*b2*cz*np.sin(gamma*np.pi/180)))
+basis = simu.triclinic_to_basis(alpha, beta, gamma, b1, b2, b3)
+volume = basis[0].dot(np.cross(basis[1], basis[2]))
+print('Volume of the reciprocal unit cell: {:.6f} nm$-3$'.format(volume))
 ##############################################################
 # calculate the angle between reference_plane and test_plane #
 ##############################################################
