886d9e5178242cb9f7d6e9920318ba699037091a
Jerome Carnis <jerome.carnis@desy.de>
2020-07-27 21:04:30 +0200

    Refactor: add a check for the length of q_range

--- bcdi/xcca/scripts/xcca_3D_map_polar.py
+++ bcdi/xcca/scripts/xcca_3D_map_polar.py
@@ -92,6 +92,7 @@ def main(calc_self, user_comment):
     global corr_count, current_point
     assert len(origin_qspace) == 3, "origin_qspace should be a tuple of 3 integer pixel values"
     assert type(calc_self) is bool, "unexpected typoe for calc_self"
+    assert len(q_range) > 1, "at least 2 values are needed for q_range"
 
     print('the CCF map will be calculated for {:d} q values: '.format(len(q_range)))
     for idx in range(len(q_range)):
