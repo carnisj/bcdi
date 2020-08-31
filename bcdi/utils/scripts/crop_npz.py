4bcc89144183ae91c4e9fe02c985aa13a6647b95
Jerome Carnis <jerome.carnis@desy.de>
2020-08-31 11:53:48 +0200

    Refactor: use module function to load the data, plot the cropped result

--- bcdi/utils/scripts/crop_npz.py
+++ bcdi/utils/scripts/crop_npz.py
@@ -6,32 +6,49 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import tkinter as tk
+from tkinter import filedialog
+sys.path.append('D:/myscripts/bcdi/')
+import bcdi.utils.utilities as util
+import bcdi.graph.graph_utils as gu
+import bcdi.postprocessing.postprocessing_utils as pu
+
 helptext = """
 Crop a stacked 3D dataset saved in NPZ format, to the desired region of interest.
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-sys.path.append('D:/myscripts/bcdi/')
-import bcdi.postprocessing.postprocessing_utils as pu
-
-homedir = "D:/data/P10_isosurface/data/p21_00038/pynxraw/"  # parent folder of scans folders
-datadir = homedir  # + 'pynxraw/'
-roi_center = [80, 278, 280]  # center of the region of interest
-output_shape = [160, 512, 256]  # shape of the output file
+homedir = "/nfs/fs/fscxi/experiments/2020/PETRA/P10/11008562/raw/"  # parent folder of scans folders
+datadir = homedir  + 'ht_pillar3_combined/pynxraw/'
+roi_center = [675, 470, 296]  # center of the region of interest
+output_shape = [100, 100, 100]  # shape of the output file
 load_mask = True  # True to load the mask and crop it
-comment = '_1_2_2'  # should start with _
-################################
+is_orthogonal = True  # True if the data is in an orthogonal frame, only used for plots
+reciprocal_space = True  # True if the data is in reciprocal space, only used for plots
+debug = False  # True to see more plots
+comment = '_2_2_2'  # should start with _
+##################################
+# end of user-defined parameters #
+##################################
 
-npzfile = np.load(datadir + 'combined_pynx_S38_S41_160_512_256_1_2_2.npz')
-try:
-    data = npzfile['data']
-except KeyError:
-    print('Wrong key, available keys:', list(npzfile.keys()))
-    sys.exit()
+###################
+# define colormap #
+###################
+colormap = gu.Colormap()
+my_cmap = colormap.cmap
 
-data = pu.crop_pad(data, output_shape=output_shape, crop_center=roi_center, debugging=True)
+#################
+# load the data #
+#################
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the data file",
+                                       filetypes=[("NPZ", "*.npz"), ("CXI", "*.cxi"), ("HDF5", "*.h5") ])
+data, _ = util.load_file(file_path)
+
+data = pu.crop_pad(data, output_shape=output_shape, crop_center=roi_center, debugging=debug)
 comment = str(output_shape[0]) + '_' + str(output_shape[1]) + '_' + str(output_shape[2]) + comment + '.npz'
 np.savez_compressed(datadir + 'combined_pynx_' + comment, data=data)
 
@@ -44,6 +61,10 @@ if load_mask:
         sys.exit()
     mask = pu.crop_pad(mask, output_shape=output_shape, crop_center=roi_center, debugging=False)
     np.savez_compressed(datadir + 'combined_maskpynx_' + comment, mask=mask)
+
+fig, _, _ = gu.multislices_plot(data, sum_frames=True, scale='log', plot_colorbar=True, vmin=0,
+                                title='Cropped data', is_orthogonal=is_orthogonal,
+                                reciprocal_space=reciprocal_space)
 
 print('End of script')
 plt.ioff()
