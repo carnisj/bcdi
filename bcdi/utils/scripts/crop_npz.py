01ad5a42c610d950886784de3db522df957201a5
Jerome Carnis <jerome.carnis@desy.de>
2020-08-31 12:01:29 +0200

    Feature: crop also optionally the q values

--- bcdi/utils/scripts/crop_npz.py
+++ bcdi/utils/scripts/crop_npz.py
@@ -25,6 +25,7 @@ datadir = homedir  + 'ht_pillar3_combined/pynxraw/'
 roi_center = [675, 470, 296]  # center of the region of interest
 output_shape = [100, 100, 100]  # shape of the output file
 load_mask = True  # True to load the mask and crop it
+load_qvalues = True  # True to load the q values and crop it
 is_orthogonal = True  # True if the data is in an orthogonal frame, only used for plots
 reciprocal_space = True  # True if the data is in reciprocal space, only used for plots
 debug = False  # True to see more plots
@@ -53,14 +54,22 @@ comment = str(output_shape[0]) + '_' + str(output_shape[1]) + '_' + str(output_s
 np.savez_compressed(datadir + 'combined_pynx_' + comment, data=data)
 
 if load_mask:
-    npzfile = np.load(datadir + 'combined_maskpynx_S38_S41_160_512_256_1_2_2.npz')
-    try:
-        mask = npzfile['mask']
-    except KeyError:
-        print('Wrong key, available keys:', list(npzfile.keys()))
-        sys.exit()
+    file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the mask file",
+                                           filetypes=[("NPZ", "*.npz"), ("CXI", "*.cxi"), ("HDF5", "*.h5")])
+    mask, _ = util.load_file(file_path)
     mask = pu.crop_pad(mask, output_shape=output_shape, crop_center=roi_center, debugging=False)
     np.savez_compressed(datadir + 'combined_maskpynx_' + comment, mask=mask)
+
+if load_qvalues:
+    file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the file containing q values",
+                                           filetypes=[("NPZ", "*.npz")])
+    q_values = np.load(file_path)
+    qx = q_values['qx']  # 1D array
+    qy = q_values['qy']  # 1D array
+    qz = q_values['qz']  # 1D array
+    qx = pu.crop_pad_1d(qx, output_shape[0], crop_center=roi_center[0])  # qx along z
+    qy = pu.crop_pad_1d(qy, output_shape[2], crop_center=roi_center[1])  # qy along x
+    qz = pu.crop_pad_1d(qz, output_shape[1], crop_center=roi_center[2])  # qz along y
 
 fig, _, _ = gu.multislices_plot(data, sum_frames=True, scale='log', plot_colorbar=True, vmin=0,
                                 title='Cropped data', is_orthogonal=is_orthogonal,
