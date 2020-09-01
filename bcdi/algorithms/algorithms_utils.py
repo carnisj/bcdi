d1d81004115afec54cf79d7f6f6389f25401220d
Jerome Carnis <jerome.carnis@desy.de>
2020-09-01 12:19:18 +0200

    Refactor: remove unused parameter

--- bcdi/algorithms/algorithms_utils.py
+++ bcdi/algorithms/algorithms_utils.py
@@ -44,9 +44,8 @@ def deconvolution_rl(image, psf=None, psf_shape=(10, 10, 10), iterations=20, deb
         image = abs(image) / abs(image) .max()
         im_deconv = abs(im_deconv) / abs(im_deconv) .max()
         gu.combined_plots(tuple_array=(image, im_deconv), tuple_sum_frames=False, tuple_colorbar=True,
-                          tuple_scale='linear', tuple_width_v=None, tuple_width_h=None, tuple_vmin=0, tuple_vmax=1,
-                          tuple_title=('Before RL', 'After '+str(iterations)+' iterations of RL (normalized)'))
-
+                          tuple_title=('Before RL', 'After '+str(iterations)+' iterations of RL (normalized)'),
+                          tuple_scale='linear', tuple_vmin=0, tuple_vmax=1)
     return im_deconv
 
 
