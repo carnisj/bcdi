a2fa0b48a057f958dcfa940fc56df5b365a0dabd
Jerome Carnis <jerome.carnis@desy.de>
2020-10-24 14:18:49 +0200

    Refactor: use banker's rounding instead of integer truncation, add a parameter to calculate the strain with phase offset when there is a defect

--- bcdi/postprocessing/scripts/strain.py
+++ bcdi/postprocessing/scripts/strain.py
@@ -43,35 +43,37 @@ Therefore the data structure is data[qx, qz, qy] for reciprocal space,
 or data[z, y, x] for real space
 """
 
-scan = 9  # spec scan number
+scan = 2290  # spec scan number
 
-datadir = "D:/data/Pt THH ex-situ/Data/HS4670/S" + str(scan) + "/pynxraw/test/"
+datadir = "D:/data/Pt THH ex-situ/Data/CH4760/S" + str(scan) + "/pynxraw/test/"
 
 sort_method = 'variance/mean'  # 'mean_amplitude' or 'variance' or 'variance/mean' or 'volume', metric for averaging
 correlation_threshold = 0.90
 #########################################################
 # parameters relative to the FFT window and voxel sizes #
 #########################################################
-original_size = [400, 512, 360]  # size of the FFT array before binning. It will be modify to take into account binning
+original_size = [240, 450, 432]  # size of the FFT array before binning. It will be modify to take into account binning
 # during phasing automatically. Leave it to () if the shape did not change.
 binning = (1, 1, 1)  # binning factor applied during phasing
-output_size = (100, 100, 100)  # (z, y, x) Fix the size of the output array, leave it as () otherwise
+output_size = (200, 200, 200)  # (z, y, x) Fix the size of the output array, leave it as () otherwise
 keep_size = False  # True to keep the initial array size for orthogonalization (slower), it will be cropped otherwise
-fix_voxel = 6  # voxel size in nm for the interpolation during the geometrical transformation
+fix_voxel = 3  # voxel size in nm for the interpolation during the geometrical transformation
 # put np.nan to use the default voxel size (mean of the voxel sizes in 3 directions)
 plot_margin = (60, 60, 60)  # (z, y, x) margin in pixel to leave outside the support in each direction when cropping,
 # it can be negative. It is useful in order to avoid cutting the object during the orthogonalization.
 #############################################################
 # parameters related to displacement and strain calculation #
 #############################################################
-isosurface_strain = 0.35  # threshold use for removing the outer layer (strain is undefined at the exact surface voxel)
+isosurface_strain = 0.48  # threshold use for removing the outer layer (strain is undefined at the exact surface voxel)
+strain_method = 'defect'  # 'default' or 'defect'. If 'defect', will offset the phase in a loop and keep the smallest
+# value for the strain (Felix Hofmann's method 2019)
 phase_offset = 0  # manual offset to add to the phase, should be 0 in most cases
 offset_origin = []  # the phase at this pixels will be set to phase_offset, leave it as [] to use offset_method instead
 offset_method = 'mean'  # 'COM' or 'mean', method for removing the offset in the phase
 centering_method = 'max_com'  # 'com' (center of mass), 'max', 'max_com' (max then com), 'do_nothing'
 # TODO: where is q for energy scans? Should we just rotate the reconstruction to have q along one axis,
 #  instead of using sample offsets?
-comment = '_gap_iso' + str(isosurface_strain)  # should start with _
+comment = '_defect_iso' + str(isosurface_strain)  # should start with _
 #################################
 # define the experimental setup #
 #################################
@@ -79,17 +81,17 @@ beamline = "ID01"  # name of the beamline, used for data loading and normalizati
 # supported beamlines: 'ID01', 'SIXS_2018', 'SIXS_2019', 'CRISTAL', 'P10', '34ID'
 rocking_angle = "outofplane"  # "outofplane" or "inplane", does not matter for energy scan
 #  "inplane" e.g. phi @ ID01, mu @ SIXS "outofplane" e.g. eta @ ID01
-sdd = 1.26  # 1.26  # sample to detector distance in m
+sdd = 0.50678  # 1.26  # sample to detector distance in m
 pixel_size = 55e-6  # detector pixel size in m, taking into account an eventual binning during preprocessing
 energy = 9000  # x-ray energy in eV, 6eV offset at ID01
 beam_direction = np.array([1, 0, 0])  # incident beam along z
-outofplane_angle = 41.1238  # detector delta ID01, delta SIXS, gamma 34ID
-inplane_angle = 0.0862  # detector nu ID01, gamma SIXS, tth 34ID
+outofplane_angle = 35.2694  # detector delta ID01, delta SIXS, gamma 34ID
+inplane_angle = -2.5110  # detector nu ID01, gamma SIXS, tth 34ID
 grazing_angle = 0  # in degrees, incident angle for in-plane rocking curves (eta ID01, th 34ID, beta SIXS)
-tilt_angle = 0.005  # angular step size for rocking angle, eta ID01, mu SIXS, does not matter for energy scan
-correct_refraction = True  # True for correcting the phase shift due to refraction
-correct_absorption = True  # True for correcting the amplitude for absorption
-optical_path_method = 'defect'  # 'threshold' or 'defect', if 'threshold' it uses isosurface_strain to define the
+tilt_angle = 0.01016  # angular step size for rocking angle, eta ID01, mu SIXS, does not matter for energy scan
+correct_refraction = False  # True for correcting the phase shift due to refraction
+correct_absorption = False  # True for correcting the amplitude for absorption
+optical_path_method = 'threshold'  # 'threshold' or 'defect', if 'threshold' it uses isosurface_strain to define the
 # support  for the optical path calculation, if 'defect' (holes) it tries to remove only outer layers even if
 # the amplitude is lower than isosurface_strain inside the crystal
 dispersion = 4.1184E-05  # delta
@@ -108,14 +110,14 @@ simu_flag = False  # set to True if it is simulation, the parameter invert_phase
 invert_phase = True  # True for the displacement to have the right sign (FFT convention), False only for simulations
 flip_reconstruction = False  # True if you want to get the conjugate object
 phase_ramp_removal = 'gradient'  # 'gradient'  # 'gradient' or 'upsampling', 'gradient' is much faster
-threshold_gradient = 1.0  # upper threshold of the gradient of the phase, use for ramp removal
+threshold_gradient = 0.1  # upper threshold of the gradient of the phase, use for ramp removal
 xrayutils_ortho = False  # True if the data is already orthogonalized
 save_raw = False  # True to save the amp-phase.vti before orthogonalization
 save_support = False  # True to save the non-orthogonal support for later phase retrieval
 save_labframe = False  # True to save the data in the laboratory frame (before rotations)
 save = True  # True to save amp.npz, phase.npz, strain.npz and vtk files
 debug = False  # set to True to show all plots for debugging
-roll_modes = (-1, 0, 0)   # axis=(0, 1, 2), correct a roll of few pixels after the decomposition into modes in PyNX
+roll_modes = (0, 0, 0)   # axis=(0, 1, 2), correct a roll of few pixels after the decomposition into modes in PyNX
 ############################################
 # parameters related to data visualization #
 ############################################
@@ -593,11 +595,12 @@ if len(offset_origin) == 0:  # use offset_method to remove the phase offset
     support[amp > isosurface_strain*amp.max()] = 1
     if offset_method == 'COM':
         zcom, ycom, xcom = center_of_mass(support)
-        print("\nOrthogonal COM in pixels (z, y, x): (", int(zcom), ',', int(ycom), ',', int(xcom), ')')
+        zcom, ycom, xcom = int(np.rint(zcom)), int(np.rint(ycom)), int(np.rint(xcom))
+        print("\nOrthogonal COM in pixels (z, y, x): ", zcom, ycom, xcom)
         print("Orthogonal phase offset at COM(amp) of:",
-              str('{:.2f}'.format(phase[int(zcom), int(ycom), int(xcom)])), "rad")
+              str('{:.2f}'.format(phase[zcom, ycom, xcom])), "rad")
 
-        phase = phase - phase[int(zcom), int(ycom), int(xcom)] + phase_offset
+        phase = phase - phase[zcom, ycom, xcom] + phase_offset
 
     elif offset_method == 'mean':
         phase = phase - phase[support == 1].mean() + phase_offset
@@ -607,8 +610,7 @@ if len(offset_origin) == 0:  # use offset_method to remove the phase offset
     del support
     gc.collect()
 else:
-    if len(offset_origin) != 3:
-        sys.exit('Invalid setting for parameter "offset_origin", [z,y,x] pixel position expected')
+    assert len(offset_origin) == 3, 'Invalid setting for parameter "offset_origin", [z,y,x] pixel position expected'
     print("\nOrthogonal phase offset at voxel ", offset_origin, " of",
           str('{:.2f}'.format(phase[offset_origin[0], offset_origin[1], offset_origin[2]])), "rad")
     phase = phase - phase[offset_origin[0], offset_origin[1], offset_origin[2]] + phase_offset
@@ -618,15 +620,14 @@ phase = pru.wrap(obj=phase, start_angle=-extent_phase / 2, range_angle=extent_ph
 if len(offset_origin) == 0:
     if offset_method == 'COM':
         print("Orthogonal phase offset after wrapping at COM(amp) of:",
-              str('{:.2f}'.format(phase[int(zcom), int(ycom), int(xcom)])), "rad")
+              str('{:.2f}'.format(phase[zcom, ycom, xcom])), "rad")
 else:
     print("Orthogonal phase offset after wrapping at voxel ", offset_origin, " of",
           str('{:.2f}'.format(phase[offset_origin[0], offset_origin[1], offset_origin[2]])), "rad")
 
-if True:
-    gu.multislices_plot(phase, width_z=2 * zrange, width_y=2 * yrange, width_x=2 * xrange,
-                        sum_frames=False, plot_colorbar=True,
-                        title='Orthogonal phase after offset removal')
+gu.multislices_plot(phase, width_z=2 * zrange, width_y=2 * yrange, width_x=2 * xrange,
+                    sum_frames=False, plot_colorbar=True,
+                    title='Orthogonal phase after offset removal')
 
 ################################
 # save to VTK before rotations #
@@ -659,8 +660,14 @@ if not xrayutils_ortho:
 ################################################################
 # calculate the strain depending on which axis q is aligned on #
 ################################################################
-strain = pu.get_strain(phase=phase, planar_distance=planar_dist, voxel_size=voxel_size,
-                       reference_axis=ref_axis_q)
+support = np.zeros(amp.shape)
+support[amp > isosurface_strain*amp.max()] = 1
+zcom, ycom, xcom = center_of_mass(support)
+del support
+gc.collect()
+
+strain = pu.get_strain(phase=phase, planar_distance=planar_dist, voxel_size=voxel_size, reference_axis=ref_axis_q,
+                       extent_phase=extent_phase, method=strain_method, debugging=debug)
 
 ################################################################
 # rotates the crystal inplane for easier slicing of the result #
