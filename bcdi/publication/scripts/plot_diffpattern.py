1b0ed2688adbacef9be824e2ec660a2eaff57a57
Jerome Carnis <jerome.carnis@desy.de>
2020-10-28 11:57:45 +0100

    Refactor: implement the option to load q_values

--- bcdi/publication/scripts/plot_diffpattern.py
+++ bcdi/publication/scripts/plot_diffpattern.py
@@ -8,66 +8,81 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
 import tkinter as tk
 from tkinter import filedialog
 from scipy.ndimage.measurements import center_of_mass
+import sys
+sys.path.append('D:/myscripts/bcdi/')
+import bcdi.graph.graph_utils as gu
+import bcdi.utils.utilities as util
 
 helptext = """
 Template for figures of the following article: 
 Carnis et al. Scientific Reports 9, 17357 (2019) https://doi.org/10.1038/s41598-019-53774-2
-Open an npz file (3D diffraction pattern) and save individual figures
+For simulated data or experimental data, open a npz file (3D diffraction pattern) and save individual figures.
+q values can be provided optionally.
 """
 
-
-scan = 2227  # spec scan number
-datadir = 'D:/data/BCDI_isosurface/S2227/oversampling/real_space_interpolation/sdd_1,01/'
-savedir = 'D:/data/BCDI_isosurface/S2227/oversampling/real_space_interpolation/sdd_1,01/'
+scan = 85  # spec scan number
+root_folder = "D:/data/test_FuzzyGridder/"
+sample_name = "S"
+datadir = root_folder + sample_name + str(scan) + '/pynx/'
+savedir = datadir
+load_qvalues = True  # True to load the q values. It expects a single npz file with fieldnames 'qx', 'qy' and 'qz'
 colorbar_range = [-1, 6]  # [vmin, vmax] log scale in photon counts
+grey_background = True  # True to set nans to grey in the plots
 save_YZ = False  # True to save the strain in YZ plane
 save_XZ = False  # True to save the strain in XZ plane
 save_XY = True  # True to save the strain in XY plane
 save_sum = False  # True to save the summed diffraction pattern in the detector, 0 to save the central slice only
-save_colorbar = True  # True to save the colorbar
 zoom_halfwidth_XY = 20  # 25  # number of pixels to crop around the Bragg peak in the detector plane, put 0 if no zoom
 zoom_halfwidth_Z = 20  # 25  # number of pixels to crop around the Bragg peak along the rocking angle, put 0 if no zoom
 comment = '_' + str(colorbar_range)  # should start with _
 if zoom_halfwidth_XY != 0:
     comment = comment + '_zoom_' + str(zoom_halfwidth_XY)
-######################################
-# define a colormap
-cdict = {'red':  ((0.0, 1.0, 1.0),
-                  (0.11, 0.0, 0.0),
-                  (0.36, 0.0, 0.0),
-                  (0.62, 1.0, 1.0),
-                  (0.87, 1.0, 1.0),
-                  (1.0, 0.0, 0.0)),
-         'green': ((0.0, 1.0, 1.0),
-                   (0.11, 0.0, 0.0),
-                   (0.36, 1.0, 1.0),
-                   (0.62, 1.0, 1.0),
-                   (0.87, 0.0, 0.0),
-                   (1.0, 0.0, 0.0)),
-         'blue': ((0.0, 1.0, 1.0),
-                  (0.11, 1.0, 1.0),
-                  (0.36, 1.0, 1.0),
-                  (0.62, 0.0, 0.0),
-                  (0.87, 0.0, 0.0),
-                  (1.0, 0.0, 0.0))}
-my_cmap = LinearSegmentedColormap('my_colormap', cdict, 256)
+##################################
+# end of user-defined parameters #
+##################################
 
-#######################################
+###################
+# define colormap #
+###################
+if grey_background:
+    bad_color = '0.7'
+else:
+    bad_color = '1.0'  # white background
+colormap = gu.Colormap(bad_color=bad_color)
+my_cmap = colormap.cmap
+
+#############
+# load data #
+#############
 plt.ion()
 root = tk.Tk()
 root.withdraw()
-file_path = filedialog.askopenfilename(initialdir=datadir, filetypes=[("NPZ", "*.npz")])
-npzfile = np.load(file_path)
-rawdata = npzfile['data']  # bulk is the amplitude minus the surface voxel layer were the strain is not defined
+file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the diffraction pattern",
+                                       filetypes=[("NPZ", "*.npz")])
+rawdata, _ = util.load_file(file_path)
 numz, numy, numx = rawdata.shape
 zcom, ycom, xcom = center_of_mass(rawdata)
 zcom, ycom, xcom = [int(np.rint(zcom)), int(np.rint(ycom)), int(np.rint(xcom))]
-print("Initial data size: (", numz, ',', numy, ',', numx, ')')
+print('Initial data shape:', rawdata.shape)
 
+################################
+# optionally load the q values #
+################################
+if load_qvalues:
+    file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the q values",
+                                           filetypes=[("NPZ", "*.npz")])
+    q_values = np.load(file_path)
+    qx = q_values['qx']
+    qz = q_values['qz']
+    qy = q_values['qy']
+    print('qx shape:', qx.shape, 'qz shape:', qz.shape, 'qy shape:', qy.shape)
+
+############################
+# plot the different views #
+############################
 if save_sum:
     comment = comment + '_sum'
     if save_XY:
@@ -83,9 +98,8 @@ if save_sum:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_XY.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
 
     if save_XZ:
         data = np.copy(rawdata)
@@ -100,9 +114,8 @@ if save_sum:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_XZ.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
 
     if save_YZ:
         data = np.copy(rawdata)
@@ -117,9 +130,8 @@ if save_sum:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_YZ.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
 else:
     if save_XY:
         fig, ax0 = plt.subplots(1, 1)
@@ -133,9 +145,8 @@ else:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_XY.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
 
     if save_XZ:
         fig, ax0 = plt.subplots(1, 1)
@@ -149,9 +160,8 @@ else:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_XZ.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
 
     if save_YZ:
         fig, ax0 = plt.subplots(1, 1)
@@ -165,8 +175,7 @@ else:
         ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
                         labelbottom=False, labelleft=False)
         plt.savefig(savedir + 'diffpattern' + comment + '_YZ.png', bbox_inches="tight")
-        if save_colorbar:
-            plt.colorbar(plt0, ax=ax0)
-            plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
+        plt.colorbar(plt0, ax=ax0)
+        plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
 plt.ioff()
 plt.show()
