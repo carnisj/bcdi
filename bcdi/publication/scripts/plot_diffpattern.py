f0d672ba45e66d57cb566d11e4024779ba9108d3
Jerome Carnis <jerome.carnis@desy.de>
2020-10-28 14:14:07 +0100

    Refactor: create a single parameter for the plotting range, remove duplicated code

--- bcdi/publication/scripts/plot_diffpattern.py
+++ bcdi/publication/scripts/plot_diffpattern.py
@@ -31,19 +31,24 @@ savedir = datadir
 load_qvalues = True  # True to load the q values. It expects a single npz file with fieldnames 'qx', 'qy' and 'qz'
 colorbar_range = [-1, 6]  # [vmin, vmax] log scale in photon counts
 grey_background = True  # True to set nans to grey in the plots
-save_YZ = False  # True to save the strain in YZ plane
-save_XZ = False  # True to save the strain in XZ plane
+save_YZ = True  # True to save the strain in YZ plane
+save_XZ = True  # True to save the strain in XZ plane
 save_XY = True  # True to save the strain in XY plane
-save_sum = False  # True to save the summed diffraction pattern in the detector, 0 to save the central slice only
-zoom_halfwidth_XY = 20  # 25  # number of pixels to crop around the Bragg peak in the detector plane, put 0 if no zoom
-zoom_halfwidth_Z = 20  # 25  # number of pixels to crop around the Bragg peak along the rocking angle, put 0 if no zoom
+save_sum = False  # True to save the summed diffraction pattern in the detector, False to save the central slice only
+plot_symmetrical = False  # if False, will not use the parameter half_range
+half_range = (None, None, None)  # tuple of three pixel numbers, half-range in each direction. Use None to use the
+# maximum symmetrical data range along one direction e.g. [20, None, None]
 comment = '_' + str(colorbar_range)  # should start with _
-if zoom_halfwidth_XY != 0:
-    comment = comment + '_zoom_' + str(zoom_halfwidth_XY)
 ##################################
 # end of user-defined parameters #
 ##################################
 
+####################
+# Check parameters #
+####################
+if save_sum:
+    comment = comment + '_sum'
+numticks_colorbar = colorbar_range[1] - colorbar_range[0] + 1
 ###################
 # define colormap #
 ###################
@@ -62,11 +67,34 @@ root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the diffraction pattern",
                                        filetypes=[("NPZ", "*.npz")])
-rawdata, _ = util.load_file(file_path)
-numz, numy, numx = rawdata.shape
-zcom, ycom, xcom = center_of_mass(rawdata)
-zcom, ycom, xcom = [int(np.rint(zcom)), int(np.rint(ycom)), int(np.rint(xcom))]
-print('Initial data shape:', rawdata.shape)
+data, _ = util.load_file(file_path)
+print('Initial data shape:', data.shape)
+
+############################
+# Check the plotting range #
+############################
+try:
+    assert len(half_range) == 3, 'half-range should be a tuple of three pixel numbers'
+except TypeError:
+    raise TypeError('half-range should be a tuple of three pixel numbers')
+
+nbz, nby, nbx = data.shape
+zcom, ycom, xcom = center_of_mass(data)
+zcom, ycom, xcom = int(np.rint(zcom)), int(np.rint(ycom)), int(np.rint(xcom))
+
+plot_range = []
+if plot_symmetrical:
+    max_range = (min(zcom, nbz-zcom), min(zcom, nbz-zcom),
+                 min(ycom, nby-ycom), min(ycom, nby-ycom),
+                 min(xcom, nbx-xcom), min(xcom, nbx-xcom))  # maximum symmetric half ranges
+else:
+    max_range = (zcom, nbz-zcom, ycom, nby-ycom, xcom, nbx-xcom)  # asymmetric half ranges
+
+for idx, val in enumerate(half_range):
+    plot_range.append(min(val or max_range[2*idx], max_range[2*idx]))
+    plot_range.append(min(val or max_range[2*idx+1], max_range[2*idx+1]))
+print('Plotting symmetrical ranges:', plot_symmetrical)
+print('Plotting range:', plot_range)
 
 ################################
 # optionally load the q values #
@@ -79,103 +107,58 @@ if load_qvalues:
     qz = q_values['qz']
     qy = q_values['qy']
     print('qx shape:', qx.shape, 'qz shape:', qz.shape, 'qy shape:', qy.shape)
+    assert (*qx.shape, *qz.shape, *qy.shape) == data.shape, 'q values and data shape are incompatible'
 
 ############################
 # plot the different views #
 ############################
-if save_sum:
-    comment = comment + '_sum'
-    if save_XY:
-        data = np.copy(rawdata)
-        data = data.sum(axis=0)
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(data[ycom-zoom_halfwidth_XY:ycom+zoom_halfwidth_XY,
-                                            xcom-zoom_halfwidth_XY:xcom+zoom_halfwidth_XY]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(data), cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XY.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
+if save_XY:
+    fig, ax0 = plt.subplots(1, 1)
+    if save_sum:
+        plt0 = ax0.imshow(np.log10(data[ycom-plot_range[2]:ycom+plot_range[3],
+                                        xcom-plot_range[4]:xcom+plot_range[5]]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    else:
+        plt0 = ax0.imshow(np.log10(data[zcom, ycom - plot_range[2]:ycom + plot_range[3],
+                                        xcom - plot_range[4]:xcom + plot_range[5]]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
+                    labelbottom=False, labelleft=False)
+    plt.savefig(savedir + 'diffpattern' + comment + '_XY.png', bbox_inches="tight")
+    gu.colorbar(plt0, numticks=numticks_colorbar)
+    plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
 
-    if save_XZ:
-        data = np.copy(rawdata)
-        data = data.sum(axis=1)
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(data[zcom-zoom_halfwidth_Z:zcom+zoom_halfwidth_Z,
-                                            xcom-zoom_halfwidth_XY:xcom+zoom_halfwidth_XY]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(data), cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XZ.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
+if save_XZ:
+    fig, ax0 = plt.subplots(1, 1)
+    if save_sum:
+        plt0 = ax0.imshow(np.log10(data[zcom-plot_range[0]:zcom+plot_range[1],
+                                        xcom-plot_range[4]:xcom+plot_range[5]]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    else:
+        plt0 = ax0.imshow(np.log10(data[zcom - plot_range[0]:zcom + plot_range[1], ycom,
+                                        xcom - plot_range[4]:xcom + plot_range[5]]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
+                    labelbottom=False, labelleft=False)
+    plt.savefig(savedir + 'diffpattern' + comment + '_XZ.png', bbox_inches="tight")
+    gu.colorbar(plt0, numticks=numticks_colorbar)
+    plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
 
-    if save_YZ:
-        data = np.copy(rawdata)
-        data = data.sum(axis=2)
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(data[zcom-zoom_halfwidth_Z:zcom+zoom_halfwidth_Z,
-                                            ycom-zoom_halfwidth_XY:ycom+zoom_halfwidth_XY]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(data), cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_YZ.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
-else:
-    if save_XY:
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(rawdata[zcom, ycom-zoom_halfwidth_XY:ycom+zoom_halfwidth_XY,
-                                               xcom-zoom_halfwidth_XY:xcom+zoom_halfwidth_XY]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(rawdata[zcom, :, :]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XY.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XY_colorbar.png', bbox_inches="tight")
+if save_YZ:
+    fig, ax0 = plt.subplots(1, 1)
+    if save_sum:
+        plt0 = ax0.imshow(np.log10(data[zcom-plot_range[0]:zcom+plot_range[1],
+                                        ycom-plot_range[2]:ycom+plot_range[3]]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    else:
+        plt0 = ax0.imshow(np.log10(data[zcom - plot_range[0]:zcom + plot_range[1],
+                                        ycom - plot_range[2]:ycom + plot_range[3], xcom]),
+                          cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
+    ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
+                    labelbottom=False, labelleft=False)
+    plt.savefig(savedir + 'diffpattern' + comment + '_YZ.png', bbox_inches="tight")
+    gu.colorbar(plt0, numticks=numticks_colorbar)
+    plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
 
-    if save_XZ:
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(rawdata[zcom - zoom_halfwidth_Z:zcom + zoom_halfwidth_Z, ycom,
-                                               xcom - zoom_halfwidth_XY:xcom + zoom_halfwidth_XY]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(rawdata[:, ycom, :]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XZ.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_XZ_colorbar.png', bbox_inches="tight")
-
-    if save_YZ:
-        fig, ax0 = plt.subplots(1, 1)
-        if zoom_halfwidth_XY != 0:
-            plt0 = ax0.imshow(np.log10(rawdata[zcom - zoom_halfwidth_Z:zcom + zoom_halfwidth_Z,
-                                               ycom - zoom_halfwidth_XY:ycom + zoom_halfwidth_XY, xcom]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        else:
-            plt0 = ax0.imshow(np.log10(rawdata[:, :, xcom]),
-                              cmap=my_cmap, vmin=colorbar_range[0], vmax=colorbar_range[1])
-        ax0.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False,
-                        labelbottom=False, labelleft=False)
-        plt.savefig(savedir + 'diffpattern' + comment + '_YZ.png', bbox_inches="tight")
-        plt.colorbar(plt0, ax=ax0)
-        plt.savefig(savedir + 'diffpattern' + comment + '_YZ_colorbar.png', bbox_inches="tight")
 plt.ioff()
 plt.show()
