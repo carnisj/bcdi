af283c0c00d37015c5fe9a25b76309b33f7e5949
Jerome Carnis <jerome.carnis@desy.de>
2020-11-07 10:24:45 +0100

    Refactor: add parameters for the plotting range, region of interest

--- bcdi/preprocessing/scripts/read_bcdi_data.py
+++ bcdi/preprocessing/scripts/read_bcdi_data.py
@@ -25,17 +25,16 @@ It is usefull when you want to localize the Bragg peak for ROI determination.
 Supported beamlines: ESRF ID01, PETRAIII P10, SOLEIL SIXS, SOLEIL CRISTAL.
 """
 
-scan = 329
-root_folder = "D:/data/Nanomax/"
-sample_name = ""  # string in front of the scan number in the folder name
-savedir = ''  # images will be saved here, leave it to '' otherwise (default to data directory's parent)
-save_mask = True  # set to True to save the mask
-fit_rockingcurve = True  # set to True if you want a fit of the rocking curve
-debug = True  # True to see more plots
+scan = 11
+root_folder = "D:/data/Pt THH ex-situ/Data/CH4760/"
+sample_name = "S"  # string in front of the scan number in the folder name
+savedir = None  # images will be saved here, leave it to None otherwise (default to data directory's parent)
+save_mask = False  # set to True to save the mask
+debug = False  # True to see more plots
 ###############################
 # beamline related parameters #
 ###############################
-beamline = 'NANOMAX'  # name of the beamline, used for data loading and normalization by monitor
+beamline = 'ID01'  # name of the beamline, used for data loading and normalization by monitor
 # supported beamlines: 'ID01', 'SIXS_2018', 'SIXS_2019', 'CRISTAL', 'P10', 'NANOMAX'
 
 custom_scan = False  # True for a stack of images acquired without scan, e.g. with ct in a macro (no info in spec file)
@@ -47,9 +46,9 @@ custom_motors = {"eta": np.linspace(16.989, 18.989, num=100, endpoint=False), "p
 # P10: om, phi, chi, mu, gamma, delta
 # SIXS: beta, mu, gamma, delta
 
-rocking_angle = "inplane"  # "outofplane" or "inplane"
+rocking_angle = "outofplane"  # "outofplane" or "inplane"
 is_series = False  # specific to series measurement at P10
-specfile_name = ''
+specfile_name = 'l5'
 # .spec for ID01, .fio for P10, alias_dict.txt for SIXS_2018, not used for CRISTAL and SIXS_2019
 # template for ID01: name of the spec file without '.spec'
 # template for SIXS_2018: full path of the alias dictionnary 'alias_dict.txt', typically: root_folder + 'alias_dict.txt'
@@ -60,19 +59,27 @@ specfile_name = ''
 ###############################
 # detector related parameters #
 ###############################
-detector = "Merlin"    # "Eiger2M" or "Maxipix" or "Eiger4M" or 'Merlin'
+detector = "Maxipix"    # "Eiger2M" or "Maxipix" or "Eiger4M" or 'Merlin'
 bragg_position = []  # Bragg peak position [vertical, horizontal], leave it as [] if there is a single peak
-peak_method = 'max'  # Bragg peak determination: 'max', 'com' or 'maxcom'.
-high_threshold = 100000  # everything above will be considered as hotpixel
-hotpixels_file = root_folder + 'merlin_mask_190222_14keV.h5'  #
-flatfield_file = ''  # root_folder + "flatfield_8.5kev.npz"  #
-template_imagefile = '%06d.h5'
+peak_method = 'maxcom'  # Bragg peak determination: 'max', 'com' or 'maxcom'.
+high_threshold = 150000  # everything above will be considered as hotpixel
+hotpixels_file = ''  # root_folder + 'merlin_mask_190222_14keV.h5'  #
+flatfield_file = root_folder + "flatfield_maxipix_8kev.npz"  #
+template_imagefile = 'data_mpx4_%05d.edf.gz'
 # template for ID01: 'data_mpx4_%05d.edf.gz' or 'align_eiger2M_%05d.edf.gz'
 # template for SIXS_2018: 'align.spec_ascan_mu_%05d.nxs'
 # template for SIXS_2019: 'spare_ascan_mu_%05d.nxs'
 # template for Cristal: 'S%d.nxs'
 # template for P10: '_master.h5'
 # template for NANOMAX: '%06d.h5'
+######################
+# setup for the plot #
+######################
+vmin = 0  # min of the colorbar (log scale)
+vmax = 6  # max of the colorbar (log scale)
+low_threshold = 1  # everthing <= 1 will be set to 0 in the plot
+width = [50, 50]  # [vertical, horizontal]
+# half width in pixels of the region of interest centered on the peak for the plot
 ##################################
 # end of user-defined parameters #
 ##################################
@@ -110,8 +117,7 @@ else:
     homedir = root_folder + sample_name + str(scan) + '/'
     detector.datadir = homedir + "data/"
 
-if savedir == '':
-    savedir = os.path.abspath(os.path.join(detector.datadir, os.pardir)) + '/'
+savedir = savedir or os.path.abspath(os.path.join(detector.datadir, os.pardir)) + '/'
 
 detector.savedir = savedir
 print('savedir: ', savedir)
@@ -141,7 +147,7 @@ if high_threshold != 0:
 ######################################################
 # calculate rocking curve and fit it to get the FWHM #
 ######################################################
-if data.ndim == 3 and fit_rockingcurve:
+if data.ndim == 3:
     tilt, _, _, _ = pru.motor_values(frames_logical=frames_logical, logfile=logfile, scan_number=scan, setup=setup,
                                      follow_bragg=False)
     rocking_curve = np.zeros(numz)
@@ -185,12 +191,20 @@ if data.ndim == 3 and fit_rockingcurve:
     ax0.legend(('data', 'interpolation'))
     plt.pause(0.1)
 
+    # apply low threshold
+    data[data <= low_threshold] = 0
+    # data = data[data.shape[0]//2, :, :]  # select the first frame e.g. for detector mesh scan
+    data = data.sum(axis=0)  # concatenate along the axis of the rocking curve
+    title = f'data.sum(axis=0)   peak method={peak_method}\n'
+else:  # 2D
+    _, y0, x0 = pru.find_bragg(data, peak_method=peak_method)
+    # apply low threshold
+    data[data <= low_threshold] = 0
+    title = f'peak method={peak_method}\n'
+
 ############################################
 # plot mask, monitor and concatenated data #
 ############################################
-# data = data[data.shape[0]//2, :, :]  # select the first frame e.g. for detector mesh scan
-data = data.sum(axis=0)  # concatenate along the axis of the rocking curve
-
 if save_mask:
     np.savez_compressed(detector.savedir + 'hotpixels.npz', mask=mask)
 
@@ -199,12 +213,19 @@ gu.combined_plots(tuple_array=(monitor, mask), tuple_sum_frames=False, tuple_sum
                   tuple_vmax=np.nan, tuple_title=('monitor', 'mask'), tuple_scale='linear', cmap=my_cmap,
                   ylabel=('Counts (a.u.)', ''))
 
-y0, x0 = np.unravel_index(abs(data).argmax(), data.shape)
-print("Max at (y, x): ", y0, x0, ' Max = ', int(data[y0, x0]))
+max_y, max_x = np.unravel_index(abs(data).argmax(), data.shape)
+print("Max at (y, x): ", max_y, max_x, ' Max = ', int(data[max_y, max_x]))
 
+# check the width for plotting the region of interest centered on the peak
+width[0] = min(width[0], y0, numy-y0)
+width[1] = min(width[1], x0, numx-x0)
+
+# plot the region of interest centered on the peak
+# extent (left, right, bottom, top)
 fig, ax = plt.subplots(nrows=1, ncols=1)
-plot = ax.imshow(np.log10(data), vmin=-2, vmax=4, cmap=my_cmap)
-ax.set_title('data.sum(axis=0)\nMax at (y, x): (' + str(y0) + ',' + str(x0) + ')   Max = ' + str(int(data[y0, x0])))
+plot = ax.imshow(np.log10(data[y0-width[0]:y0+width[0], x0-width[1]:x0+width[1]]), vmin=vmin, vmax=vmax, cmap=my_cmap,
+                 extent=[x0-width[1], x0+width[1], y0+width[0], y0-width[0]])
+ax.set_title(f'{title} Peak at (y, x): ({y0},{x0})   Peak value = {int(data[y0, x0])}')
 if beamline == 'NANOMAX':
     ax.invert_yaxis()  # the detector is mounted upside-down on the robot arm at Nanomax
 gu.colorbar(plot)
