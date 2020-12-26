301ce64957b4e8019e268882d682cf904b0a9c13
Jerome Carnis <jerome.carnis@desy.de>
2020-12-26 21:47:33 +0100

    Feature: add tests for the validation module

--- bcdi/test/tests.py
+++ bcdi/test/tests.py
@@ -6,3 +6,35 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 
+import unittest
+import bcdi.utils.validation as valid
+
+
+def run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    runner = unittest.TextTestRunner(verbosity=2)
+    return runner.run(suite)
+
+
+class TestValidation(unittest.TestCase):
+    def setUp(self):
+        print('Running setup...')
+
+    def tearDown(self):
+        print('Running tear down...')
+
+    def test_list(self):
+        self.assertTrue(valid.valid_container(list(), container_types=list))
+
+    def test_tuple(self):
+        self.assertTrue(valid.valid_container(tuple(), container_types=tuple))
+
+    def test_set(self):
+        self.assertTrue(valid.valid_container(set(), container_types=set))
+
+    # self.assertNotEqual(tz, test_tz)
+
+
+if __name__ == 'main':
+    result = run_tests(TestValidation)
+    print(result)
