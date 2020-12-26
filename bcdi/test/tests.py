d49ba588ad3c406cedb5695f93d9f6c27dd6b02e
Jerome Carnis <jerome.carnis@desy.de>
2020-12-26 20:38:39 +0100

    Initial add

---
+++ bcdi/test/tests.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-present : DESY PHOTON SCIENCE
+#       authors:
+#         Jerome Carnis, carnis_jerome@yahoo.fr
+
