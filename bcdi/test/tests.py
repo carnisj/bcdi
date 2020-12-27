db35474dc6fde3a045af0ceea9aa86579412060c
Jerome Carnis <jerome.carnis@desy.de>
2020-12-27 21:33:55 +0100

    Feature: add tests for valid_kwargs

--- bcdi/test/tests.py
+++ bcdi/test/tests.py
@@ -18,88 +18,151 @@ def run_tests(test_class):
 
 class TestValidation(unittest.TestCase):
     # def setUp(self):
-    #     print('Running setup...')
+    #     executed before each test
     #
     # def tearDown(self):
-    #     print('Running tear down...')
+    #     executed after each test
 
     ############################
-    # tests on valid.container #
+    # tests on valid_container #
     ############################
-    def test_container_type(self):
+    # valid_container(obj, container_types, length=None, min_length=None, item_types=None, min_included=None,
+    #                 min_excluded=None, max_included=None, max_excluded=None, allow_none=False, name=None)
+
+    def test_validcontainer_container_type(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=1)
 
-    def test_container_list(self):
+    def test_validcontainer_container_list(self):
         self.assertTrue(valid.valid_container(list(), container_types=list))
 
-    def test_container_tuple(self):
+    def test_validcontainer_container_tuple(self):
         self.assertTrue(valid.valid_container(tuple(), container_types=tuple))
 
-    def test_container_set(self):
+    def test_validcontainer_container_set(self):
         self.assertTrue(valid.valid_container(set(), container_types=set))
 
-    def test_container_none(self):
+    def test_validcontainer_container_none(self):
         self.assertRaises(ValueError, valid.valid_container, obj=list(), container_types=None)
 
-    def test_container_dict(self):
-        self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=dict)
+    def test_validcontainer_container_wrong_type(self):
+        self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types={dict, tuple})
 
-    def test_container_types(self):
-        self.assertTrue(valid.valid_container(obj=list(), container_types=list, item_types=(list, tuple)))
-
-    def test_container_length_1(self):
+    def test_validcontainer_container_length_float(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, length=2.3)
 
-    def test_container_length_2(self):
+    def test_validcontainer_container_length_negative(self):
         self.assertRaises(ValueError, valid.valid_container, obj=list(), container_types=list, length=-2)
 
-    def test_container_length_3(self):
+    def test_validcontainer_container_length_null(self):
         self.assertRaises(ValueError, valid.valid_container, obj=list(), container_types=list, length=0)
 
-    def test_container_minlength_1(self):
+    def test_validcontainer_container_minlength_float(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, min_length=2.3)
 
-    def test_container_minlength_2(self):
+    def test_validcontainer_container_minlength_negative(self):
         self.assertRaises(ValueError, valid.valid_container, obj=list(), container_types=list, length=-2)
 
-    def test_container_itemtype_1(self):
-        self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, item_types=2)
-
-    def test_container_itemtype_2(self):
+    def test_validcontainer_container_itemtype(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, item_types=int))
 
-    def test_container_itemtype_3(self):
+    def test_validcontainer_container_itemtype_none(self):
+        self.assertTrue(valid.valid_container(obj=list(), container_types=list, item_types=None))
+
+    def test_validcontainer_container_itemtype_int(self):
+        self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, item_types=2)
+
+    def test_validcontainer_container_itemtype_collection(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, item_types=(int, float)))
 
-    def test_container_min_included_1(self):
+    def test_validcontainer_container_min_included(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, min_included=0))
 
-    def test_container_min_included_2(self):
+    def test_validcontainer_container_min_included_complex(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, min_included=1+1j)
 
-    def test_container_min_excluded_1(self):
+    def test_validcontainer_container_min_excluded(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, min_excluded=0))
 
-    def test_container_min_excluded_2(self):
+    def test_validcontainer_container_min_excluded_complex(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, min_excluded=1+1j)
 
-    def test_container_max_included_1(self):
+    def test_validcontainer_container_max_included(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, max_included=0))
 
-    def test_container_max_included_2(self):
+    def test_validcontainer_container_max_included_complex(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, max_included=1+1j)
 
-    def test_container_max_excluded_1(self):
+    def test_validcontainer_container_max_excluded(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, max_excluded=0))
 
-    def test_container_max_excluded_2(self):
+    def test_validcontainer_container_max_excluded_complex(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, max_excluded=1+1j)
 
-    def test_container_allownone_1(self):
+    def test_validcontainer_container_allownone(self):
         self.assertTrue(valid.valid_container(obj=list(), container_types=list, allow_none=True))
 
-    def test_container_allownone_2(self):
+    def test_validcontainer_container_allownone_int(self):
         self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, allow_none=0)
+
+    def test_validcontainer_container_allownone_none(self):
+        self.assertRaises(TypeError, valid.valid_container, obj=list(), container_types=list, allow_none=None)
+
+    def test_validcontainer_length(self):
+        self.assertTrue(valid.valid_container(obj=[1, 2], container_types=list, length=2))
+
+    def test_validcontainer_wrong_length(self):
+        self.assertRaises(ValueError, valid.valid_container, obj=[1, 2], container_types=list, length=3)
+
+    def test_validcontainer_minlength(self):
+        self.assertTrue(valid.valid_container(obj=[1, 2], container_types=list, min_length=2))
+
+    def test_validcontainer_wrong_minlength(self):
+        self.assertRaises(ValueError, valid.valid_container, obj=[1, 2], container_types=list, min_length=3)
+
+    def test_validcontainer_allownone(self):
+        self.assertTrue(valid.valid_container(obj=[1, None], container_types=list, allow_none=True))
+
+    def test_validcontainer_notallownone(self):
+        self.assertRaises(ValueError, valid.valid_container, obj=[1, None], container_types=list, allow_none=False)
+
+    #########################
+    # tests on valid_kwargs #
+    #########################
+    # valid_kwargs(kwargs, allowed_kwargs, name=None)
+
+    def test_validkwargs_kwargs_type_dict(self):
+        self.assertTrue(valid.valid_kwargs(kwargs=dict(), allowed_kwargs='test'))
+
+    def test_validkwargs_wrong_kwargs_none(self):
+        self.assertRaises(TypeError, valid.valid_kwargs, kwargs=None, allowed_kwargs='test')
+
+    def test_validkwargs_wrong_kwargs_type(self):
+        self.assertRaises(TypeError, valid.valid_kwargs, kwargs=[1, 2, 3], allowed_kwargs='test')
+
+    def test_validkwargs_allowed_kwargs_empty(self):
+        self.assertRaises(ValueError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs=list())
+
+    def test_validkwargs_allowed_kwargs_dict(self):
+        self.assertRaises(TypeError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs={'test': 1})
+
+    def test_validkwargs_allowed_kwargs_length(self):
+        self.assertRaises(ValueError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs='')
+
+    def test_validkwargs_allowed_kwargs_items_length(self):
+        self.assertRaises(ValueError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs=('', 'test', 'moon'))
+
+    def test_validkwargs_allowed_kwargs_items_type(self):
+        self.assertRaises(TypeError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs=(1, 'test', 'moon'))
+
+    def test_validkwargs_allowed_kwargs_items_none(self):
+        self.assertRaises(TypeError, valid.valid_kwargs, kwargs=dict(), allowed_kwargs=('test', None))
+
+    def test_validkwargs_valid_kwargs(self):
+        self.assertTrue(valid.valid_kwargs(kwargs={'test': 0, 'red': None}, allowed_kwargs={'test', 'red'}))
+
+    def test_validkwargs_invalid_kwargs(self):
+        self.assertRaises(KeyError, valid.valid_kwargs, kwargs={'test': 0, 'red': None},
+                          allowed_kwargs={'test', 'blue'})
 
 
 if __name__ == 'main':
