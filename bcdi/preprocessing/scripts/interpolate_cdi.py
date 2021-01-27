49875da9d32839ba50940df09fda2862f9ab7231
Jerome Carnis <jerome.carnis@desy.de>
2021-01-27 17:27:52 +0100

    Refactor: save figures for the final data and mask

--- bcdi/preprocessing/scripts/interpolate_cdi.py
+++ bcdi/preprocessing/scripts/interpolate_cdi.py
@@ -166,9 +166,10 @@ def main(parameters):
     if plot:
         gu.multislices_plot(array=data, sum_frames=True, plot_colorbar=True, scale='log', slice_position=origin,
                             is_orthogonal=True, reciprocal_space=True, vmin=0, title='data after interpolation')
+        plt.savefig(savedir + 'centrosym_finalsum' + comment + '.png')
         gu.multislices_plot(array=mask, sum_frames=True, plot_colorbar=False, scale='linear', slice_position=origin,
                             is_orthogonal=True, reciprocal_space=True, vmin=0, title='mask after interpolation')
-
+        plt.savefig(savedir + 'centrosym_finalmask' + comment + '.png')
     ##################################
     # save the updated data and mask #
     ##################################
