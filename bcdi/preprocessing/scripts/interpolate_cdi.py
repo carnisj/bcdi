88563c2fad21cb01241de028f789bab34f3d2487
Jerome Carnis <jerome.carnis@desy.de>
2021-01-27 09:37:11 +0100

    Feature: implement multiprocessing

--- bcdi/preprocessing/scripts/interpolate_cdi.py
+++ bcdi/preprocessing/scripts/interpolate_cdi.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 import sys
 import tkinter as tk
 from tkinter import filedialog
@@ -23,91 +24,152 @@ identical voxel sizes in all directions. The mask should be an array of integers
 diffraction pattern. 
 """
 
-datadir = ''  # location of the data and mask
-savedir = None  # path where to save the result, will default to datadir if None
-comment = ''  # comment for the file name when saving, should start with _
-origin = (12, 23, 15)  # tuple of three integers, position in pixels of the origin of reciprocal space
-plot = True  # True to show plots of the data and mask, before and after the interpolation
+data_dir = ''  # location of the data and mask
+save_dir = None  # path where to save the result, will default to datadir if None
+user_comment = ''  # comment for the file name when saving, should start with _
+origin_voxel = (12, 23, 15)  # tuple of three integers, position in pixels of the origin of reciprocal space
+plot_data = True  # True to show plots of the data and mask, before and after the interpolation
 ##################################
 # end of user-defined parameters #
 ##################################
 
-###################
-# define colormap #
-###################
-colormap = gu.Colormap()
-my_cmap = colormap.cmap
+##############################################
+# create the dictionnary of input parameters #
+##############################################
+params = {'datadir': data_dir, 'savedir': save_dir, 'user_comment': user_comment, 'origin': origin_voxel,
+          'plot': plot_data}
 
-###################################
-# load experimental data and mask #
-###################################
-plt.ion()
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the diffraction pattern",
-                                       filetypes=[("NPZ", "*.npz"), ("NPY", "*.npy")])
-data, _ = util.load_file(file_path)
-data = data.astype(float)
 
-file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the mask",
-                                       filetypes=[("NPZ", "*.npz"), ("NPY", "*.npy")])
-mask, _ = util.load_file(file_path)
-mask = mask.astype(int)
+def check_voxel(index, mask_array, masked_voxels, ref_voxel, datarange):
+    """
 
-#########################
-# check some parameters #
-#########################
-savedir = savedir or datadir
 
-if data.shape != mask.shape:
-    raise ValueError(f'Incompatible shape for the data: {data.shape} and the mask: {mask.shape}')
-
-if data.ndim != 3:
-    raise ValueError('only 3D data is supported')
-
-valid.valid_container(obj=origin, container_types=(tuple, list, np.ndarray), item_types=int, length=3,
-                      name='interpolate_cdi.py')
-
-nbz, nby, nbx = data.shape
-# calculate the range of pixels indices covered by the data, taking into account the origin of reciprocal space
-data_extent = (-origin[0], nbz-origin[0]-1, -origin[1], nby-origin[1]-1, -origin[2], nbx-origin[2]-1)
-
-###################################################
-# plot the data and mask before the interpolation #
-###################################################
-if plot:
-    gu.multislices_plot(array=data, sum_frames=False, plot_colorbar=True, scale='log', slice_position=origin,
-                        is_orthogonal=True, reciprocal_space=True, vmin=0, title='data before interpolation')
-    gu.multislices_plot(array=mask, sum_frames=False, plot_colorbar=False, scale='linear', slice_position=origin,
-                        is_orthogonal=True, reciprocal_space=True, vmin=0, vmax=1, title='mask before interpolation')
-
-####################################################################################################################
-# loop over masked points to see if the centrosymmetric voxel is also masked, if not copy its intensity and unmask #
-####################################################################################################################
-ind_z, ind_y, ind_x = np.nonzero(mask)
-
-for idx in range(len(ind_z)):
+    :param index:
+    :param mask_array:
+    :param masked_voxels:
+    :param ref_voxel:
+    :param datarange:
+    :return:
+    """
+    # position of the masked voxel
+    pos_z, pos_y, pos_x = masked_voxels[0][index], masked_voxels[1][index], masked_voxels[2][index]
     # calculate the position of the centrosymmetric voxel
-    sym_z, sym_y, sym_x = 2 * origin[0] - ind_z[idx], 2 * origin[1] - ind_y[idx], 2 * origin[2] - ind_x[idx]
+    sym_z, sym_y, sym_x = (2 * ref_voxel[0] - pos_z,
+                           2 * ref_voxel[1] - pos_y,
+                           2 * ref_voxel[2] - pos_x)
 
     # check if this voxel is masked. Copy its intensity if not.
-    if util.in_range(point=(sym_z, sym_y, sym_x), extent=data_extent) and not mask[sym_z, sym_y, sym_x]:
-        data[ind_z[idx], ind_y[idx], ind_x[idx]] = data[sym_z, sym_y, sym_x]
-        mask[ind_z[idx], ind_y[idx], ind_x[idx]] = 0
+    if util.in_range(point=(sym_z, sym_y, sym_x), extent=datarange) and not mask_array[sym_z, sym_y, sym_x]:
+        return True, (pos_z, pos_y, pos_x), (sym_z, sym_y, sym_x)
+    return False, None, None
 
-##################################################
-# plot the data and mask after the interpolation #
-##################################################
-if plot:
-    gu.multislices_plot(array=data, sum_frames=False, plot_colorbar=True, scale='log', slice_position=origin,
-                        is_orthogonal=True, reciprocal_space=True, vmin=0, title='data after interpolation')
-    gu.multislices_plot(array=mask, sum_frames=False, plot_colorbar=False, scale='linear', slice_position=origin,
-                        is_orthogonal=True, reciprocal_space=True, vmin=0, vmax=1, title='mask after interpolation')
 
-##################################
-# save the updated data and mask #
-##################################
-np.savez_compressed(savedir + 'centrosym_data' + comment, data=data)
-np.savez_compressed(savedir + 'centrosym_data' + comment, mask=mask)
-plt.ioff()
-plt.show()
+def main(parameters):
+    """
+    Protection for multiprocessing.
+
+    :param parameters: dictionnary containing input parameters
+    """
+
+    def collect_result(result):
+        """
+        Callback processing the result after asynchronous multiprocessing. Update the global arrays.
+
+        :param result: tuple output of check_voxel, (boolean, masked voxel indices, centrosymmetric voxel indices).
+         The boolean will be True if the centrosymmetrix voxel intensity can be used
+        """
+        nonlocal data, mask, current_point, nb_points
+        current_point += 1
+        if result[0]:  # True
+            data[result[1][0], result[1][1], result[1][2]] = data[result[2][0], result[2][1], result[2][2]]
+            mask[result[1][0], result[1][1], result[1][2]] = 0
+        if (current_point % 10000) == 0:
+            sys.stdout.write('\rPoint {:d} / {:d}'.format(current_point, nb_points))
+            sys.stdout.flush()
+
+    ######################################
+    # load the dictionnary of parameters #
+    ######################################
+    datadir = parameters['datadir']
+    savedir = parameters['savedir']
+    comment = parameters['comment']
+    origin = parameters['origin']
+    plot = parameters['plot']
+
+    ###################################
+    # load experimental data and mask #
+    ###################################
+    plt.ion()
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the diffraction pattern",
+                                           filetypes=[("NPZ", "*.npz"), ("NPY", "*.npy")])
+    data, _ = util.load_file(file_path)
+    data = data.astype(float)
+
+    file_path = filedialog.askopenfilename(initialdir=datadir, title="Select the mask",
+                                           filetypes=[("NPZ", "*.npz"), ("NPY", "*.npy")])
+    mask, _ = util.load_file(file_path)
+    mask = mask.astype(int)
+
+    #########################
+    # check some parameters #
+    #########################
+    savedir = savedir or datadir
+
+    if data.shape != mask.shape:
+        raise ValueError(f'Incompatible shape for the data: {data.shape} and the mask: {mask.shape}')
+
+    if data.ndim != 3:
+        raise ValueError('only 3D data is supported')
+
+    valid.valid_container(obj=origin, container_types=(tuple, list, np.ndarray), item_types=int, length=3,
+                          name='interpolate_cdi.py')
+
+    nbz, nby, nbx = data.shape
+    # calculate the range of pixels indices covered by the data, taking into account the origin of reciprocal space
+    data_extent = (-origin[0], nbz-origin[0]-1, -origin[1], nby-origin[1]-1, -origin[2], nbx-origin[2]-1)
+
+    ###################################################
+    # plot the data and mask before the interpolation #
+    ###################################################
+    if plot:
+        gu.multislices_plot(array=data, sum_frames=False, plot_colorbar=True, scale='log', slice_position=origin,
+                            is_orthogonal=True, reciprocal_space=True, vmin=0, title='data before interpolation')
+        gu.multislices_plot(array=mask, sum_frames=False, plot_colorbar=False, scale='linear', slice_position=origin,
+                            is_orthogonal=True, reciprocal_space=True, vmin=0, vmax=1,
+                            title='mask before interpolation')
+
+    ####################################################################################################################
+    # loop over masked points to see if the centrosymmetric voxel is also masked, if not copy its intensity and unmask #
+    ####################################################################################################################
+    nonzero_mask = np.nonzero(mask)  # tuple of three 1D arrays (ind_z, ind_y, ind_x)
+    current_point = 0
+    nb_points = len(nonzero_mask[0])
+
+    print(f'\nNumber of processors used: {mp.cpu_count()}')
+    mp.freeze_support()
+    pool = mp.Pool(processes=mp.cpu_count())  # use this number of processes
+    for idx in range(nb_points):
+        pool.apply_async(check_voxel, args=(idx, nonzero_mask, origin, data_extent), callback=collect_result,
+                         error_callback=util.catch_error)
+
+    pool.close()
+    pool.join()  # postpones the execution of next line of code until all processes in the queue are done.
+
+    ##################################################
+    # plot the data and mask after the interpolation #
+    ##################################################
+    if plot:
+        gu.multislices_plot(array=data, sum_frames=False, plot_colorbar=True, scale='log', slice_position=origin,
+                            is_orthogonal=True, reciprocal_space=True, vmin=0, title='data after interpolation')
+        gu.multislices_plot(array=mask, sum_frames=False, plot_colorbar=False, scale='linear', slice_position=origin,
+                            is_orthogonal=True, reciprocal_space=True, vmin=0, vmax=1, title='mask after interpolation')
+
+    ##################################
+    # save the updated data and mask #
+    ##################################
+    np.savez_compressed(savedir + 'centrosym_data' + comment, data=data)
+    np.savez_compressed(savedir + 'centrosym_data' + comment, mask=mask)
+    plt.ioff()
+    plt.show()
