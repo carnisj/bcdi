bfc882c3ec9791001ea88a0b3004a98bb40fe919
Jerome Carnis <jerome.carnis@desy.de>
2021-03-15 10:26:41 +0100

    Refactor: allow loading 2D SEM data

--- bcdi/postprocessing/scripts/line_profile.py
+++ bcdi/postprocessing/scripts/line_profile.py
@@ -10,6 +10,7 @@ import json
 import matplotlib.pyplot as plt
 from numbers import Real
 import numpy as np
+import os
 import pathlib
 from scipy.interpolate import interp1d
 import sys
@@ -26,20 +27,21 @@ defining the object from the background. Must be given as input: the voxel size 
 the direction of the cuts and a list of points where to apply the cut along this direction.   
 """
 
-datadir = "D:/data/P10_2nd_test_isosurface_Dec2020/data_nanolab/dataset_1_newpsf/result/"  # data folder
-savedir = "D:/data/P10_2nd_test_isosurface_Dec2020/data_nanolab/dataset_1_newpsf/result/linecuts/"
+datadir = "D:/data/P10_2nd_test_isosurface_Dec2020/data_nanolab/AFM-SEM/SEM calibration/"  # data folder
+savedir = "D:/data/P10_2nd_test_isosurface_Dec2020/data_nanolab/AFM-SEM/SEM calibration/test/"
 # results will be saved here, if None it will default to datadir
-threshold = np.linspace(0, 1.0, num=20)
+threshold = 0
 # number or list of numbers between 0 and 1, modulus threshold defining the normalized object from the background
-direction = (0, 1, 0)  # tuple of 2 or 3 numbers (2 for 2D object, 3 for 3D) defining the direction of the cut
+direction = (0, 1)  # tuple of 2 or 3 numbers (2 for 2D object, 3 for 3D) defining the direction of the cut
 # in the orthonormal reference frame is given by the array axes. It will be corrected for anisotropic voxel sizes.
-points = {(25, 37, 23), (25, 37, 24), (25, 37, 25), (25, 37, 26),
-          (26, 37, 23), (26, 37, 24), (26, 37, 25), (26, 37, 26),
-          (27, 37, 24), (27, 37, 25)}
+points = {(0, 5), (0, 25), (0, 50), (0, 75), (0, 100), (0, 125), (0, 150), (0, 175), (0, 200), (0, 225)}
+#  {(25, 37, 23), (25, 37, 24), (25, 37, 25), (25, 37, 26),
+#      (26, 37, 23), (26, 37, 24), (26, 37, 25), (26, 37, 26),
+#      (27, 37, 24), (27, 37, 25)}
 # list/tuple/set of 2 or 3 indices (2 for 2D object, 3 for 3D) corresponding to the points where
 # the cut alond direction should be performed. The reference frame is given by the array axes.
-voxel_size = 5  # positive real number  or tuple of 2 or 3 positive real number (2 for 2D object, 3 for 3D)
-width_lines = (98, 100, 102)  # list of vertical lines that will appear in the plot width vs threshold
+voxel_size = 4.140786749482402  # positive real number  or tuple of 2 or 3 positive real number (2 for 2D object, 3 for 3D)
+width_lines = None  # (98, 100, 102)  # list of vertical lines that will appear in the plot width vs threshold, None otherwise
 styles = {0: (0, (2, 6)), 1: 'dashed', 2: (0, (2, 6))}  # line style for the width_lines, 1 for each line
 debug = False  # True to print the output dictionary and plot the legend
 comment = ''  # string to add to the filename when saving
@@ -64,9 +66,13 @@ root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(initialdir=datadir,
                                        filetypes=[("NPZ", "*.npz"), ("NPY", "*.npy"),
-                                                  ("CXI", "*.cxi"), ("HDF5", "*.h5")])
+                                                  ("CXI", "*.cxi"), ("HDF5", "*.h5"), ("all files", "*.*")])
 
-obj, _ = util.load_file(file_path)
+_, ext = os.path.splitext(file_path)
+if ext in {'.png', '.jpg', '.tif'}:
+    obj = util.image_to_ndarray(filename=file_path, convert_grey=True, cmap='gray', debug=False)
+else:
+    obj, _ = util.load_file(file_path)
 ndim = obj.ndim
 
 #########################
@@ -99,14 +105,19 @@ valid.valid_container(threshold, container_types=(list, tuple, np.ndarray), item
 if isinstance(width_lines, Real):
     width_lines = (width_lines,)
 valid.valid_container(width_lines, container_types=(list, tuple, np.ndarray), item_types=Real,
-                      min_excluded=0, name='line_profile')
+                      min_excluded=0, allow_none=True, name='line_profile')
 
-if not isinstance(styles, dict):
-    raise TypeError('styles should be a dictionnary')
-if len(styles) != len(width_lines):
-    raise ValueError('styles should have as many entries as the number of width_lines')
+if width_lines is not None:
+    tmp_thres = np.zeros((len(width_lines), len(points)))
+    if not isinstance(styles, dict):
+        raise TypeError('styles should be a dictionnary')
+    if len(styles) != len(width_lines):
+        raise ValueError('styles should have as many entries as the number of width_lines')
 
-comment = f'_direction{direction[0]}_{direction[1]}_{direction[2]}_{comment}'
+if ndim == 3:
+    comment = f'_direction{direction[0]}_{direction[1]}_{direction[2]}_{comment}'
+else:
+    comment = f'_direction{direction[0]}_{direction[1]}_{comment}'
 
 #########################
 # normalize the modulus #
@@ -160,7 +171,8 @@ fig.savefig(savedir + 'cut' + comment + '_labels.png')
 #################################################################################
 # calculate the evolution of the width of the object depending on the threshold #
 #################################################################################
-for key, value in result.items():
+idx_point = 0
+for key, value in result.items():  # loop over the linecuts
     fit = interp1d(value['distance'], value['cut'])
     dist_interp = np.linspace(value['distance'].min(), value['distance'].max(), num=10000)
     cut_interp = fit(dist_interp)
@@ -174,39 +186,34 @@ for key, value in result.items():
             width[idx] = dist_interp[crossings.max()] - dist_interp[crossings.min()]
         else:
             width[idx] = 0
-
-    # fit the function width vs threshold and estimate where it crosses the expected widths
-    fit = interp1d(width, threshold)  # width vs threshold is monotonic (decreasing with increasing threshold)
-    fit_thresh = np.empty(len(width_lines))
-    for idx, val in enumerate(width_lines):
-        fit_thresh[idx] = fit(val)
     # update the dictionary value
     value['threshold'] = threshold
     value['width'] = width
-    value['fitted_threshold'] = fit_thresh
+
+    if width_lines is not None:
+        # fit the function width vs threshold and estimate where it crosses the expected widths
+        fit = interp1d(width, threshold)  # width vs threshold is monotonic (decreasing with increasing threshold)
+        for idx_line, val in enumerate(width_lines):
+            tmp_thres[idx_line, idx_point] = fit(val)
+        idx_point += 1
 
 #################################################
 # calculate statistics on the fitted thresholds #
 #################################################
-count = 0
-tmp_thres = np.zeros((len(width_lines), len(points)))
-for key, value in result.items():
-    # iterating over points, value is a dictionary
-    for idx in range(len(width_lines)):
-        tmp_thres[idx, count] = value['fitted_threshold'][idx]
-    count += 1
-mean_thres = np.mean(tmp_thres, axis=1)
-std_thres = np.std(tmp_thres, axis=1)
-
-# update the dictionary
-result['direction'] = direction
-result['expected_width'] = width_lines
-result['mean_thres'] = np.round(mean_thres, decimals=3)
-result['std_thres'] = np.round(std_thres, decimals=3)
+if width_lines is not None:
+    mean_thres = np.mean(tmp_thres, axis=1)
+    std_thres = np.std(tmp_thres, axis=1)
+    # update the dictionary
+    result['fitted_thresholds'] = tmp_thres
+    result['expected_width'] = width_lines
+    result['mean_thres'] = np.round(mean_thres, decimals=3)
+    result['std_thres'] = np.round(std_thres, decimals=3)
 
 #################################
 #  plot the widths vs threshold #
 #################################
+result['direction'] = direction
+
 fig = plt.figure(figsize=(12, 9))
 ax = plt.subplot(111)
 plot_nb = 0
@@ -223,12 +230,13 @@ ax.spines['right'].set_linewidth(tick_width)
 ax.spines['left'].set_linewidth(tick_width)
 ax.spines['top'].set_linewidth(tick_width)
 ax.spines['bottom'].set_linewidth(tick_width)
-for index, hline in enumerate(width_lines):
-    ax.axhline(y=hline, linestyle=styles[index], color='k', linewidth=1.5)
+if width_lines is not None:
+    for index, hline in enumerate(width_lines):
+        ax.axhline(y=hline, linestyle=styles[index], color='k', linewidth=1.5)
 fig.savefig(savedir + 'width_vs_threshold' + comment + '.png')
 ymin, ymax = ax.get_ylim()
 
-ax.set_xlim(left=0.335, right=0.565)
+ax.set_xlim(left=0.27, right=0.50)  # ax.set_xlim(left=0.335, right=0.565)
 ax.set_ylim(bottom=96, top=104)
 fig.savefig(savedir + 'width_vs_threshold' + comment + '_zoom.png')
 ax.tick_params(labelbottom=True, labelleft=True, axis='both', which='major', labelsize=16)
@@ -242,9 +250,10 @@ ax.set_ylabel('width (nm)', fontsize=20)
 ax.set_title(f"Width vs threshold in the direction {result['direction']}\n", fontsize=20)
 if debug:
     ax.legend(fontsize=14)
-fig.text(0.15, 0.30, f"expected widths: {result['expected_width']}", size=16)
-fig.text(0.15, 0.25, f"fitted thresholds: {result['mean_thres']}", size=16)
-fig.text(0.15, 0.20, f"stds: {result['std_thres']}", size=16)
+if width_lines is not None:
+    fig.text(0.15, 0.30, f"expected widths: {result['expected_width']}", size=16)
+    fig.text(0.15, 0.25, f"fitted thresholds: {result['mean_thres']}", size=16)
+    fig.text(0.15, 0.20, f"stds: {result['std_thres']}", size=16)
 ax.tick_params(labelbottom=True, labelleft=True, axis='both', which='major', labelsize=16)
 plt.pause(0.5)
 fig.savefig(savedir + 'width_vs_threshold' + comment + '_labels.png')
