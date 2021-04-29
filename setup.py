11ce84c03d21b34fbb352afed7c9460269ceac18
Jerome Carnis <jerome.carnis@desy.de>
2021-04-29 10:22:36 +0200

    Refactor: update the list of dependencies

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(name='bcdi', version='0.1.1',
           'Documentation': 'https://bcdi.readthedocs.io/en/latest/'},
       python_requires='>=3.6',
       install_requires=['numpy', 'scipy', 'scikit-image', 'matplotlib', 'hdf5plugin', 'h5py', 'traits', 'vtk',
-                        'mayavi', 'xrayutilities', 'fabio', 'silx', 'lmfit', 'moviepy'],
+                        'importlib_resources', 'mayavi', 'xrayutilities', 'fabio', 'silx', 'lmfit', 'moviepy'],
       classifiers=[
           # How mature is this project? Common values are
           #   3 - Alpha
