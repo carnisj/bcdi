a1259ffefcc10e4a728a9650ea061c03854249a6
Jerome Carnis <jerome.carnis@desy.de>
2021-05-20 15:57:22 +0200

    Update affiliation

--- bcdi/simulation/__init__.py
+++ bcdi/simulation/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
-#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP 
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
+#   (c) 06/2021-present : DESY CFEL
 #       authors:
 #         Jerome Carnis, jerome.carnis@esrf.fr
 
