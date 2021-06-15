39db134da5df67b327fbe32991c5725f4beed42d
Jerome Carnis <jerome.carnis@desy.de>
2021-06-15 09:10:17 +0200

    Refactor: PEP 8, raise error instead of using assert

--- bcdi/xcca/xcca_utils.py
+++ bcdi/xcca/xcca_utils.py
@@ -16,10 +16,11 @@ def angular_avg(
     data, q_values, mask=None, origin=None, nb_bins=np.nan, debugging=False
 ):
     """
-    Calculate an angular average of a 3D reciprocal space dataset given q values and the position of the origin of the
-    reciprocal space.
+    Calculate an angular average of a 3D reciprocal space dataset given q values and
+    the position of the origin of the reciprocal space.
 
-    :param data: 3D reciprocal space data gridded in the orthonormal frame (qx downstream, qz vertical up, qy outboard)
+    :param data: 3D reciprocal space data gridded in the orthonormal frame
+     (qx downstream, qz vertical up, qy outboard)
     :param q_values: tuple of 3 1-D arrays: (qx downstream, qz vertical up, qy outboard)
     :param mask: 3D array of the same shape as data. 1 for a masked voxel, 0 otherwise
     :param origin: position in pixels of the origin of the reciprocal space
@@ -34,7 +35,8 @@ def angular_avg(
     if mask is None:
         mask = np.zeros(data.shape)
     else:
-        assert mask.shape == data.shape, "mask should have the same shape as data"
+        if mask.shape != data.shape:
+            raise ValueError("mask and data should have the same shape")
     qx, qz, qy = q_values
     nz, ny, nx = data.shape
 
@@ -115,18 +117,21 @@ def angular_avg(
 
 def calc_ccf_polar(point, q1_name, q2_name, bin_values, polar_azi_int):
     """
-    Calculate for the cross-correlation of point with all other points at the second q value and sort the result.
+    Calculate for the cross-correlation of point with all other points at the second q
+    value and sort the result.
 
     :param point: the reference point
     :param q1_name: key for the first q value in the dictionnary polar_azi_int
     :param q2_name: key for the second q value in the dictionnary polar_azi_int
-    :param bin_values: in radians, angular bin values where to calculate the cross-correlation
-    :param polar_azi_int: a dictionnary with fields 'q1', 'q2', ... Each field contains three 1D
-     arrays: polar angle, azimuthal angle and intensity values for each point
-    :return: the sorted cross-correlation values, angular bins indices and number of points contributing to the angular
-     bins
+    :param bin_values: in radians, angular bin values where to calculate the
+     cross-correlation
+    :param polar_azi_int: a dictionnary with fields 'q1', 'q2', ... Each field contains
+     three 1D arrays: polar angle, azimuthal angle and intensity values for each point
+    :return: the sorted cross-correlation values, angular bins indices and number of
+     points contributing to the angular bins
     """
-    # calculate the angle between the current point and all points from the second q value (delta in [0 pi])
+    # calculate the angle between the current point and all points from the second
+    # q value (delta in [0 pi])
     delta_val = np.arccos(
         np.sin(polar_azi_int[q1_name][point, 0])
         * np.sin(polar_azi_int[q2_name][:, 0])
@@ -135,8 +140,9 @@ def calc_ccf_polar(point, q1_name, q2_name, bin_values, polar_azi_int):
         * np.cos(polar_azi_int[q2_name][:, 0])
     )
 
-    # It can happen that the value in the arccos is outside [-1, 1] because of the limited floating precision of Python,
-    # which result in delta_val = nan. These points would contribute to the 0 and 180 degrees CCF, and can be neglected.
+    # It can happen that the value in the arccos is outside [-1, 1] because of
+    # the limited floating precision of Python, which result in delta_val = nan.
+    # These points would contribute to the 0 and 180 degrees CCF, and can be neglected.
 
     # find the nearest angular bin value for each value of the array delta
     nearest_indices = util.find_nearest(
@@ -150,7 +156,8 @@ def calc_ccf_polar(point, q1_name, q2_name, bin_values, polar_azi_int):
         nearest_indices, return_counts=True
     )  # counter_indices are sorted
 
-    # filter out -1 indices which correspond to no neighbour in the range defined by width in find_nearest()
+    # filter out -1 indices which correspond to no neighbour in the range defined by
+    # width in find_nearest()
     counter_val = np.delete(counter_val, np.argwhere(counter_indices == -1))
     counter_indices = np.delete(counter_indices, np.argwhere(counter_indices == -1))
 
@@ -167,18 +174,21 @@ def calc_ccf_polar(point, q1_name, q2_name, bin_values, polar_azi_int):
 
 def calc_ccf_rect(point, q1_name, q2_name, bin_values, q_int):
     """
-    Calculate for the cross-correlation of point with all other points at the second q value and sort the result.
+    Calculate for the cross-correlation of point with all other points at the second q
+    value and sort the result.
 
     :param point: the reference point
     :param q1_name: key for the first q value in the dictionnary polar_azi_int
     :param q2_name: key for the second q value in the dictionnary polar_azi_int
-    :param bin_values: in radians, angular bin values where to calculate the cross-correlation
+    :param bin_values: in radians, angular bin values where to calculate the
+     cross-correlation
     :param q_int: a dictionnary with fields 'q1', 'q2', ... Each field contains four 1D
      arrays: qx, qy, qz and intensity values for each point
-    :return: the sorted cross-correlation values, angular bins indices and number of points contributing to the angular
-     bins
+    :return: the sorted cross-correlation values, angular bins indices and number of
+     points contributing to the angular bins
     """
-    # calculate the angle between the current point and all points from the second q value (delta in [0 pi])
+    # calculate the angle between the current point and all points from the second
+    # q value (delta in [0 pi])
     delta_val = np.arccos(
         np.divide(
             np.dot(q_int[q2_name][:, 0:3], q_int[q1_name][point, 0:3]),
@@ -186,8 +196,9 @@ def calc_ccf_rect(point, q1_name, q2_name, bin_values, q_int):
             * np.linalg.norm(q_int[q1_name][point, 0:3]),
         )
     )
-    # It can happen that the value in the arccos is outside [-1, 1] because of the limited floating precision of Python,
-    # which result in delta_val = nan. These points would contribute to the 0 and 180 degrees CCF, and can be neglected.
+    # It can happen that the value in the arccos is outside [-1, 1] because of
+    # the limited floating precision of Python, which result in delta_val = nan.
+    # These points would contribute to the 0 and 180 degrees CCF, and can be neglected.
 
     # find the nearest angular bin value for each value of the array delta
     nearest_indices = util.find_nearest(
@@ -201,7 +212,8 @@ def calc_ccf_rect(point, q1_name, q2_name, bin_values, q_int):
         nearest_indices, return_counts=True
     )  # counter_indices are sorted
 
-    # filter out -1 indices which correspond to no neighbour in the range defined by width in find_nearest()
+    # filter out -1 indices which correspond to no neighbour in the range defined by
+    # width in find_nearest()
     counter_val = np.delete(counter_val, np.argwhere(counter_indices == -1))
     counter_indices = np.delete(counter_indices, np.argwhere(counter_indices == -1))
 
@@ -214,6 +226,3 @@ def calc_ccf_rect(point, q1_name, q2_name, bin_values, q_int):
         ).sum()
 
     return ccf_uniq_val, counter_val, counter_indices
-
-
-# if __name__ == "__main__":
