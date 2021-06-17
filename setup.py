f1744bf18095d86345936c4fb7b0b4bfba6463aa
Jerome Carnis <jerome.carnis@desy.de>
2021-06-17 09:34:53 +0200

    Refactor: correct RST markup

--- setup.py
+++ setup.py
@@ -115,18 +115,20 @@ setup(
         diffraction imaging data.
         
         BCDI stands for *Bragg coherent X-ray diffraction imaging*.
+
         It can be used for:
-            1. Pre-processing BCDI and forward CDI data (masking aliens, detector gaps)
-               before phase retrieval.
-            2. Post-processing phased data (phase offset and phase ramp removal,
-               averaging, apodization...).
-            3. Data analysis on diffraction data (stereographic projection,
-               angular cross correlation analysis, domain orientation fitting ...).
-            4. Data analysis on phased data (resolution calculation, statistics on the
-               retrieved strain...).
-            5. Simulation of diffraction intensity (including noise, detector gaps,
-               displacement...).
-            6. Creating figures for publication using templates.""",
+        
+         1. Pre-processing BCDI and forward CDI data (masking aliens, detector gaps)
+            before phase retrieval.
+         2. Post-processing phased data (phase offset and phase ramp removal,
+            averaging, apodization...).
+         3. Data analysis on diffraction data (stereographic projection,
+            angular cross correlation analysis, domain orientation fitting ...).
+         4. Data analysis on phased data (resolution calculation, statistics on the
+            retrieved strain...).
+         5. Simulation of diffraction intensity (including noise, detector gaps,
+            displacement...).
+         6. Creating figures for publication using templates.""",
     url="https://github.com/carnisj/bcdi",
     project_urls={"Documentation": "https://bcdi.readthedocs.io/en/latest/"},
     python_requires=">=3.6, !=3.9.*",
