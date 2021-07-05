387aa3ef514fe634de2a6722243206d4f4134655
Jerome Carnis <jerome.carnis@desy.de>
2021-07-05 14:22:45 +0200

    PEP 8

--- bcdi/scripts/utils/bcdi_center_of_rotation.py
+++ bcdi/scripts/utils/bcdi_center_of_rotation.py
@@ -10,12 +10,14 @@
 from math import tan, pi
 
 helptext = """
-Calculate the vertical correction and correction along the beam to apply to a nanocrystal, given the drift along
-the beam at -/+ delta_angle from the center of the rocking curve.
+Calculate the vertical correction and correction along the beam to apply to a 
+nanocrystal, given the drift along the beam at -/+ delta_angle from the center of the 
+rocking curve.
 """
 
 incident_angle = 21.0326  # in degrees
-delta_angle = 0.4  # in degrees, the motor position is checked at -/+ delta_angle from the incident_angle
+delta_angle = 0.4  # in degrees, the motor position is checked at -/+ delta_angle
+# from the incident_angle
 drift_um = 2.4  # drift of the motor position along the beam, in micrometers
 
 delta_vertical = (
