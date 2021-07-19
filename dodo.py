1bbfaae5270f42f5f3c45d88562fc60d80687a25
Jerome Carnis <jerome.carnis@desy.de>
2021-07-19 16:45:28 +0200

    Feature: run tests with coverage and generate a XML coverage report

--- dodo.py
+++ dodo.py
@@ -2,17 +2,47 @@
 doit configuration for BCDI
 """
 
-
 import coverage
 import os
-import unittest
 
 
 def task_black():
-    print(
-        "Running black against the folder "
-        f"{os.path.dirname(os.path.abspath(__file__))}\n"
-    )
+    """
+    Run black against the package.
+    """
     return {
         "actions": [f"python -m black {os.path.dirname(os.path.abspath(__file__))}"],
+        "verbosity": 2,
+    }
+
+
+def task_coverage_xml():
+    """
+    Generate an XML version of the coverage report. It can be opened with Notepad++.
+    """
+
+    def create_coverage_xml(coverage_file, output_file):
+        print("XML coverage report generated in test_output/")
+        cov = coverage.Coverage(data_file=coverage_file)
+        cov.load()
+        cov.xml_report(outfile=output_file)
+
+    return {
+        "actions": [
+            (create_coverage_xml, [".coverage", "test_output/coverage-report.xml"])
+        ],
+        "file_dep": [".coverage"],
+        "targets": ["test-output/coverage-report.xml"],
+        "verbosity": 2,
+    }
+
+
+def task_tests():
+    """
+    Run unit tests with coverage.
+    """
+    return {
+        "actions": [f"coverage run --source=bcdi -m unittest discover"],
+        "targets": [".coverage", "test_output/unit-test-report.xml"],
+        "verbosity": 2,
     }
