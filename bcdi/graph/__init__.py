874e377bb8abd76c75376c6e15b7b5e8637dc3e7
Jerome Carnis <jerome.carnis@desy.de>
2021-07-21 08:36:49 +0200

    Fix docstring (pydocstyle)

--- bcdi/graph/__init__.py
+++ bcdi/graph/__init__.py
@@ -6,3 +6,8 @@
 #   (c) 06/2021-present : DESY CFEL
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
+"""
+BCDI graph.
+
+This package contains functions related to visualization.
+"""
