f93b483c37f1c5157b14dc03f3f30ea493c987c1
Jerome Carnis <jerome.carnis@desy.de>
2021-07-30 17:20:29 +0200

    Refactor: added a test relative to abc instantiation

---
+++ tests/experiment/test_diffractometer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
+#   (c) 06/2021-present : DESY CFEL
+#       authors:
+#         Jerome Carnis, carnis_jerome@yahoo.fr
+
+import unittest
+import bcdi.experiment.experiment_utils as exp
+
+
+def run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    runner = unittest.TextTestRunner(verbosity=2)
+    return runner.run(suite)
+
+
+class Test(unittest.TestCase):
+    """
+    Tests
+    """
+
+    def test_create_diffractometer_from_parent(self):
+        with self.assertRaises(TypeError):
+            exp.Diffractometer(sample_offsets=[])
+
+
+if __name__ == "main":
+    run_tests(Test)
