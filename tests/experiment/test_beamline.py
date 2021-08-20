ab43069c927a9e94899a616dc1670094044995eb
Jerome Carnis <jerome.carnis@desy.de>
2021-08-20 10:54:11 +0200

    Initial add

---
+++ tests/experiment/test_beamline.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
+#   (c) 06/2021-present : DESY CFEL
+#       authors:
+#         Jerome Carnis, carnis_jerome@yahoo.fr
+
+import unittest
+from bcdi.experiment.beamline import Beamline
+
+
+def run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    runner = unittest.TextTestRunner(verbosity=2)
+    return runner.run(suite)
+
+
+class Test(unittest.TestCase):
+    """Tests related to beamline instantiation."""
+
+    def test_create_beamline_from_abc(self):
+        with self.assertRaises(TypeError):
+            Beamline(name="ID01")
+
+
+if __name__ == "__main__":
+    run_tests(Test)
