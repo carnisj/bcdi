9f6367805a55ac6165197e3e99b812d29130a52f
Jerome Carnis <jerome.carnis@desy.de>
2021-08-20 13:50:52 +0200

    Add tests

--- tests/experiment/test_beamline.py
+++ tests/experiment/test_beamline.py
@@ -7,8 +7,9 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 
+import numpy as np
 import unittest
-from bcdi.experiment.beamline import Beamline
+from bcdi.experiment.beamline import create_beamline, Beamline
 
 
 def run_tests(test_class):
@@ -17,7 +18,7 @@ def run_tests(test_class):
     return runner.run(suite)
 
 
-class Test(unittest.TestCase):
+class TestBeamline(unittest.TestCase):
     """Tests related to beamline instantiation."""
 
     def test_create_beamline_from_abc(self):
@@ -25,5 +26,38 @@ class Test(unittest.TestCase):
             Beamline(name="ID01")
 
 
+class TestBeamlineCRISTAL(unittest.TestCase):
+    """Tests related to CRISTAL beamline instantiation."""
+
+    def setUp(self):
+        self.beamline = create_beamline("ID01")
+
+    def test_detector_hor(self):
+        self.assertTrue(
+            self.beamline.detector_hor == "y+"
+        )
+
+    def test_detector_ver(self):
+        self.assertTrue(
+            self.beamline.detector_hor == "z-"
+        )
+
+    def test_exit_wavevector(self):
+        params = {
+            "inplane_angle": 0.0,
+            "outofplane_angle": 90.0,
+            "wavelength_m": 2 * np.pi,
+        }
+        print(self.beamline.exit_wavevector(params))
+        self.assertTrue(
+            np.allclose(
+                self.beamline.exit_wavevector(params),
+                np.array([0.0, 1.0, 0.0]),
+                rtol=1e-09,
+                atol=1e-09
+            )
+        )
+
+
 if __name__ == "__main__":
-    run_tests(Test)
+    run_tests(TestBeamline)
