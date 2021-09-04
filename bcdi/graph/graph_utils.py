dd0816988a49d3356dbc757aca86ce510102146e
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-09-04 18:01:47 +0200

    remove unused variable

--- bcdi/graph/graph_utils.py
+++ bcdi/graph/graph_utils.py
@@ -1350,7 +1350,7 @@ def linecut(array, start_indices, stop_indices, interp_order=3, debugging=False)
             )
             ax1.plot(cut)
         else:
-            fig, (ax0, ax1, ax2, ax3), _ = multislices_plot(array, sum_frames=False)
+            _, (ax0, ax1, ax2, ax3), _ = multislices_plot(array, sum_frames=False)
             ax0.plot(
                 [start_indices[2], stop_indices[2]],
                 [start_indices[1], stop_indices[1]],
