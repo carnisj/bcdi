0d26a3ccbefc48236eda815e17c371b03a62b680
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-10-10 13:47:46 +0200

    add tests for the 3D case

--- tests/utils/test_image_registration.py
+++ tests/utils/test_image_registration.py
@@ -18,9 +18,9 @@ def run_tests(test_class):
     return runner.run(suite)
 
 
-class TestGetShift(unittest.TestCase):
+class TestGetShift2D(unittest.TestCase):
     """
-    Tests on the function image_registration.get_shift.
+    Tests on the function image_registration.get_shift for 2D arrays.
 
     def get_shift(
     reference_array: np.ndarray,
@@ -143,9 +143,134 @@ class TestGetShift(unittest.TestCase):
             )
 
 
-class TestShiftArray(unittest.TestCase):
+class TestGetShift3D(unittest.TestCase):
     """
-    Tests on the function image_registration.shift_array.
+    Tests on the function image_registration.get_shift for 3D arrays.
+
+    def get_shift(
+    reference_array: np.ndarray,
+    shifted_array: np.ndarray,
+    shift_method: str = "modulus",
+    precision: int = 1000,
+    support_threshold: Union[None, float] = None,
+    verbose: bool = True,
+    ) -> Sequence[float]:
+    """
+
+    def setUp(self):
+        # executed before each test
+        reference_array = np.zeros((5, 5, 5), dtype=complex)
+        reference_array[1:4, 1:4, 1:4] = 1 + 1j
+        shifted_array = np.zeros((5, 5, 5), dtype=complex)
+        shifted_array[2:, 2:, 0:3] = 1 + 1j
+        self.reference_array = reference_array
+        self.shifted_array = shifted_array
+
+    def test_method_modulus(self):
+        shifts = reg.get_shift(
+            reference_array=self.reference_array,
+            shifted_array=self.shifted_array,
+            shift_method="modulus",
+        )
+        self.assertTrue(
+            np.allclose(
+                np.asarray(shifts),
+                np.array([-1.0, -1.0, 1.0]),
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_method_raw(self):
+        shifts = reg.get_shift(
+            reference_array=self.reference_array,
+            shifted_array=self.shifted_array,
+            shift_method="raw",
+        )
+        self.assertTrue(
+            np.allclose(
+                np.asarray(shifts),
+                np.array([-1.0, -1.0, 1.0]),
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_method_support(self):
+        shifts = reg.get_shift(
+            reference_array=self.reference_array,
+            shifted_array=self.shifted_array,
+            shift_method="support",
+            support_threshold=0.5,
+        )
+        self.assertTrue(
+            np.allclose(
+                np.asarray(shifts),
+                np.array([-1.0, -1.0, 1.0]),
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_method_support_none(self):
+        with self.assertRaises(ValueError):
+            reg.get_shift(
+                reference_array=self.reference_array,
+                shifted_array=self.shifted_array,
+                shift_method="support",
+            )
+
+    def test_precision_float(self):
+        with self.assertRaises(TypeError):
+            reg.get_shift(
+                reference_array=self.reference_array,
+                shifted_array=self.shifted_array,
+                precision=2.3,
+            )
+
+    def test_precision_null(self):
+        with self.assertRaises(ValueError):
+            reg.get_shift(
+                reference_array=self.reference_array,
+                shifted_array=self.shifted_array,
+                precision=0,
+            )
+
+    def test_precision_None(self):
+        with self.assertRaises(ValueError):
+            reg.get_shift(
+                reference_array=self.reference_array,
+                shifted_array=self.shifted_array,
+                precision=None,
+            )
+
+    def test_precision_min_allowed(self):
+        shifts = reg.get_shift(
+            reference_array=self.reference_array,
+            shifted_array=self.shifted_array,
+            precision=1,
+        )
+        self.assertTrue(
+            np.allclose(
+                np.asarray(shifts),
+                np.array([-1.0, -1.0, 1.0]),
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_wrong_method_name(self):
+        with self.assertRaises(ValueError):
+            reg.get_shift(
+                reference_array=self.reference_array,
+                shifted_array=self.shifted_array,
+                shift_method="wrong",
+            )
+
+
+class TestShiftArray2D(unittest.TestCase):
+    """
+    Tests on the function image_registration.shift_array for 2D arrays.
 
     def shift_array(
     array: np.ndarray, shift: Sequence[float], interpolation_method: str = "subpixel"
@@ -218,5 +343,83 @@ class TestShiftArray(unittest.TestCase):
         )
 
 
+class TestShiftArray3D(unittest.TestCase):
+    """
+    Tests on the function image_registration.shift_array for 3D arrays.
+
+    def shift_array(
+    array: np.ndarray, shift: Sequence[float], interpolation_method: str = "subpixel"
+    ) -> np.ndarray:
+    """
+
+    def setUp(self):
+        # executed before each test
+        reference_array = np.zeros((5, 5, 5), dtype=complex)
+        reference_array[1:4, 1:4, 1:4] = 1 + 1j
+        shifted_array = np.zeros((5, 5, 5), dtype=complex)
+        shifted_array[2:, 2:, 0:3] = 1 + 1j
+        self.reference_array = reference_array
+        self.shifted_array = shifted_array
+        self.shifts = reg.get_shift(
+            reference_array=self.reference_array, shifted_array=self.shifted_array
+        )
+
+    def test_output_dtype(self):
+        aligned_array = reg.shift_array(
+            array=self.shifted_array,
+            shift=self.shifts,
+            interpolation_method="subpixel",
+        )
+        self.assertEqual(aligned_array.dtype, self.shifted_array.dtype)
+
+    def test_method_subpixel(self):
+        aligned_array = reg.shift_array(
+            array=self.shifted_array,
+            shift=self.shifts,
+            interpolation_method="subpixel",
+        )
+        self.assertTrue(
+            np.allclose(
+                self.reference_array,
+                aligned_array,
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_method_rgi(self):
+        aligned_array = reg.shift_array(
+            array=self.shifted_array,
+            shift=self.shifts,
+            interpolation_method="rgi",
+        )
+        self.assertTrue(
+            np.allclose(
+                self.reference_array,
+                aligned_array,
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+    def test_method_roll(self):
+        aligned_array = reg.shift_array(
+            array=self.shifted_array,
+            shift=self.shifts,
+            interpolation_method="roll",
+        )
+        self.assertTrue(
+            np.allclose(
+                self.reference_array,
+                aligned_array,
+                rtol=1e-09,
+                atol=1e-09,
+            )
+        )
+
+
 if __name__ == "__main__":
-    run_tests(TestGetShift)
+    run_tests(TestGetShift2D)
+    run_tests(TestGetShift2D)
+    run_tests(TestShiftArray2D)
+    run_tests(TestShiftArray3D)
