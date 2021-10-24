464d5ef821859a40a1377ecd019d4d6f86a3ec55
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-10-24 11:26:34 +0200

    put a description of the parameters in the help text

--- scripts/preprocessing/bcdi_preprocess_BCDI.py
+++ scripts/preprocessing/bcdi_preprocess_BCDI.py
@@ -46,14 +46,236 @@ filter the data.
 Beamlines currently supported: ESRF ID01, SOLEIL CRISTAL, SOLEIL SIXS, PETRAIII P10 and
 APS 34ID-C.
 
-Output: data and mask as numpy .npz or Matlab .mat 3D arrays for phasing
-
-File structure should be (e.g. scan 1):
+By default, the directory structure should be (e.g. scan 1):
 specfile, hotpixels file and flatfield file in:    /rootdir/
 data in:                                           /rootdir/S1/data/
 
 output files saved in:   /rootdir/S1/pynxraw/ or /rootdir/S1/pynx/ depending on the
 'use_rawdata' option
+
+    Parameters related to path names:
+    
+    :param scans: e.g. 11
+     scan number or list of scan numbers
+    :param root_folder: e.g. "C:/Users/Jerome/Documents/data/dataset_ID01/"
+     folder of the experiment, where all scans are stored
+    :param save_dir: e.g. "C:/Users/Jerome/Documents/data/dataset_ID01/test/"
+     images will be saved here, leave it to None otherwise
+    :param data_dir: e.g. None
+     use this to override the beamline default search path for the data
+    :param sample_name: e.g. "S"
+     str or list of str of sample names (string in front of the scan number in the
+     folder name). If only one name is indicated, it will be repeated to match the
+     number of scans.
+    :param comment: string use in filenames when saving
+    :param debug: e.g. False
+     True to see plots
+
+
+    Parameters used in the interactive masking GUI:
+
+    :param flag_interact: e.g. True
+     True to interact with plots, False to close it automatically
+    :param background_plot: e.g. "0.5"
+     background color for the GUI in level of grey in [0,1], 0 being dark. For visual
+     comfort during interactive masking.
+
+    Parameters related to data cropping/padding/centering #
+
+    :param centering_method: e.g. "max"
+     Bragg peak determination: 'max' or 'com', 'max' is better usually. It will be
+     overridden by 'fix_bragg' if not empty
+    :param fix_bragg: e.g. [121, 321, 256]
+     Bragg peak position [z_bragg, y_bragg, x_bragg] considering the full detector.
+     It is useful if hotpixels or intense aliens. Leave None otherwise.
+    :param fix_size: e.g. [0, 256, 10, 240, 50, 350]
+     crop the array to that predefined size considering the full detector.
+     [zstart, zstop, ystart, ystop, xstart, xstop], ROI will be defaulted to [] if
+     fix_size is provided. Leave None otherwise
+    :param center_fft: e.g. "skip"
+     how to crop/pad/center the data, available options: 'crop_sym_ZYX','crop_asym_ZYX',
+     'pad_asym_Z_crop_sym_YX', 'pad_sym_Z_crop_asym_YX', 'pad_sym_Z', 'pad_asym_Z',
+     'pad_sym_ZYX','pad_asym_ZYX' or 'skip'
+    :param pad_size: e.g. [256, 512, 512]
+     Use this to pad the array. Used in 'pad_sym_Z_crop_sym_YX', 'pad_sym_Z' and
+     'pad_sym_ZYX'. Leave None otherwise.
+
+    Parameters for data filtering
+
+    :param mask_zero_event: e.g. False
+    mask pixels where the sum along the rocking curve is zero may be dead pixels
+    :param median_filter: e.g. "skip"
+     which filter to apply, available filters:
+     
+     - 'median': to apply a med2filter [3,3]
+     - 'interp_isolated': to interpolate isolated empty pixels based on 'medfilt_order'
+       parameter
+     - 'mask_isolated': mask isolated empty pixels
+     - 'skip': skip filtering
+     
+    :param median_filter_order: e.g. 7
+     minimum number of non-zero neighboring pixels to apply filtering
+
+    Parameters used when reloading processed data
+
+    :param reload_previous: e.g. False
+     True to resume a previous masking (load data and mask)
+    :param reload_orthogonal: e.g. False
+     True if the reloaded data is already intepolated in an orthonormal frame
+    :param preprocessing_binning: e.g. [1, 1, 1]
+     binning factors in each dimension of the binned data to be reloaded
+
+    Options for saving:
+
+    :param save_rawdata: e.g. False
+     True to save also the raw data when use_rawdata is False
+    :param save_to_npz: e.g. True
+     True to save the processed data in npz format
+    :param save_to_mat: e.g. False
+     True to save also in .mat format
+    :param save_to_vti: e.g. False
+     True to save the orthogonalized diffraction pattern to VTK file
+    :param save_as_int: e.g. False
+     True to save the result as an array of integers (save space)
+
+    Parameters for the beamline:
+
+    :param beamline: e.g. "ID01"
+     name of the beamline, used for data loading and normalization by monitor
+    :param actuators: e.g. {'rocking_angle': 'actuator_1_1'}
+     optional dictionary that can be used to define the entries corresponding to
+     actuators in data files (useful at CRISTAL where the location of data keeps
+     changing, or to declare a non-standard monitor)
+    :param is_series: e.g. True
+     specific to series measurement at P10
+    :param custom_scan: e.g. False
+     True for a stack of images acquired without scan, e.g. with ct in a
+     macro, or when there is no spec/log file available
+    :param custom_images: list of image numbers for the custom_scan, None otherwise
+    :param custom_monitor: list of monitor values for normalization for the custom_scan,
+     None otherwise
+    :param rocking_angle: e.g. "outofplane"
+     "outofplane" for a sample rotation around x outboard, "inplane" for a sample
+     rotation around y vertical up, "energy"
+    :param specfile_name: e.g. "l5"
+     beamline-dependent parameter, use the following template:
+
+     - template for ID01 and 34ID-C: name of the spec file without '.spec'
+     - template for SIXS: full path of the alias dictionnary or None to use the one in
+       the package folder
+     - template for all other beamlines: None
+
+    Parameters for the detector:
+
+    :param detector: e.g. "Maxipix"
+     name of the detector
+    :param phasing_binning: e.g. [1, 2, 2]
+     binning to apply to the data (stacking dimension, detector vertical axis, detector
+     horizontal axis)
+    :param linearity_func: linearity correction for the detector decalred as a string,
+     leave None otherwise.
+    :param x_bragg: e.g. 1577
+     horizontal pixel number of the Bragg peak, it can be used for the definition of
+     the ROI
+    :param y_bragg: e.g. 833
+     vertical pixel number of the Bragg peak, can be used for the definition of the ROI
+    :param roi_detector: e.g.[y_bragg - 50, y_bragg + 50, x_bragg - 50, x_bragg + 50]
+     [Vstart, Vstop, Hstart, Hstop], leave None to use the full detector. Use this with
+     center_fft='skip' if you want this exact size.
+    :param normalize_flux: e.g. "monitor"
+     'monitor' to normalize the intensity by the default monitor values,
+     'skip' to do nothing
+    :param photon_threshold: e.g. 0
+     voxels with a smaller intensity will be set to 0.
+    :param photon_filter: e.g. "loading"
+     'loading' or 'postprocessing', when the photon threshold should be applied.
+     If 'loading', it is applied before binning; if 'postprocessing', it is applied at
+     the end of the script before saving
+    :param bin_during_loading: e.g. False
+     True to bin during loading, faster
+    :param frames_pattern:  list of int, of length data.shape[0].
+     If frames_pattern is 0 at index, the frame at data[index] will be skipped, if 1
+     the frame will be added to the stack. Use this if you need to remove some frames
+     and you know it in advance.
+    :param background_file: non-empty file path or None
+    :param hotpixels_file: non-empty file path or None
+    :param flatfield_file: non-empty file path or None
+    :param template_imagefile: e.g. "data_mpx4_%05d.edf.gz"
+     use one of the following template:
+
+     - template for ID01: 'data_mpx4_%05d.edf.gz' or 'align_eiger2M_%05d.edf.gz'
+     - template for SIXS_2018: 'align.spec_ascan_mu_%05d.nxs'
+     - template for SIXS_2019: 'spare_ascan_mu_%05d.nxs'
+     - template for Cristal: 'S%d.nxs'
+     - template for P10: '_master.h5'
+     - template for NANOMAX: '%06d.h5'
+     - template for 34ID: 'Sample%dC_ES_data_51_256_256.npz'
+
+    Parameters below if you want to orthogonalize the data before phasing:
+
+    :param use_rawdata: e.g. True
+     False for using data gridded in laboratory frame, True for using data in detector
+     frame
+    :param interpolation_method: e.g. "xrayutilities"
+     'xrayutilities' or 'linearization'
+    :param fill_value_mask: e.g. 0
+     0 (not masked) or 1 (masked). It will define how the pixels outside of the data
+     range are processed during the interpolation. Because of the large number of masked
+     pixels, phase retrieval converges better if the pixels are not masked (0 intensity
+     imposed). The data is by default set to 0 outside of the defined range.
+    :param beam_direction: e.g. [1, 0, 0]
+     beam direction in the laboratory frame (downstream, vertical up, outboard)
+    :param sample_offsets: e.g. None
+     tuple of offsets in degrees of the sample for each sample circle (outer first).
+     convention: the sample offsets will be subtracted to the motor values. Leave None
+     if there is no offset.
+    :param sdd: e.g. 0.50678
+     in m, sample to detector distance in m
+    :param energy: e.g. 9000
+     X-ray energy in eV, it can be a number or a list in case of energy scans.
+    :param custom_motors: e.g. {"mu": 0, "phi": -15.98, "chi": 90, "theta": 0,
+     "delta": -0.5685, "gamma": 33.3147}
+     use this to declare motor positions if there is not log file, None otherwise
+
+    Parameters when orthogonalizing the data before phasing  using the linearized
+    transformation matrix:
+
+    :param align_q: e.g. True
+     if True it rotates the crystal to align q, along one axis of the array. It is used
+     only when interp_method is 'linearization'
+    :param ref_axis_q: e.g. "y"  # q will be aligned along that axis
+    :param outofplane_angle: e.g. 42.6093
+     detector angle in deg (rotation around x outboard, typically delta), corrected for
+     the direct beam position. Leave None to use the uncorrected position.
+    :param inplane_angle: e.g. -0.5783
+     detector angle in deg(rotation around y vertical up, typically gamma), corrected
+     for the direct beam position. Leave None to use the uncorrected position.
+
+    Parameters when orthogonalizing the data before phasing  using xrayutilities.
+    xrayutilities uses the xyz crystal frame (for zero incident angle x is downstream,
+    y outboard, and z vertical up):
+
+    :param sample_inplane: e.g. [1, 0, 0]
+     sample inplane reference direction along the beam at 0 angles in xrayutilities
+     frame
+    :param sample_outofplane: e.g. [0, 0, 1]
+     surface normal of the sample at 0 angles in xrayutilities frame
+    :param offset_inplane: e.g. 0
+     outer detector angle offset as determined by xrayutilities area detector
+     initialization
+    :param cch1: e.g. 208
+     direct beam vertical position in the full unbinned detector for xrayutilities 2D
+     detector calibration
+    :param cch2: e.g. 154
+     direct beam horizontal position in the full unbinned detector for xrayutilities 2D
+     detector calibration
+    :param detrot: e.g. 0
+     detrot parameter from xrayutilities 2D detector calibration
+    :param tiltazimuth: e.g. 360
+     tiltazimuth parameter from xrayutilities 2D detector calibration
+    :param tilt_detector: e.g. 0
+     tilt parameter from xrayutilities 2D detector calibration
+
 """
 
 
@@ -78,7 +300,8 @@ def run(prm):
         """
         Function to interact with a plot, return the position of clicked pixel.
 
-        If flag_pause==1 or if the mouse is out of plot axes, it will not register the click
+        If flag_pause==1 or if the mouse is out of plot axes, it will not register
+        the click.
 
         :param event: mouse click event
         """
@@ -105,7 +328,7 @@ def run(prm):
 
     def press_key(event):
         """
-        Interact with a plot for masking parasitic diffraction intensity or detector gaps
+        Interact with a plot for masking parasitic intensity or detector gaps
 
         :param event: button press event
         """
