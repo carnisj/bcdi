e5ca43cd863ecc558ecd6469c9523e644913404e
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-10-24 14:43:51 +0200

    update the docstring

--- scripts/postprocessing/bcdi_strain.py
+++ scripts/postprocessing/bcdi_strain.py
@@ -101,6 +101,140 @@ qy] for reciprocal space, or data[z, y, x] for real space
      directions. Set it to None to use the default voxel size (calculated from q values,
      it will be different in each dimension).
 
+    Parameters related to displacement and strain calculation:
+
+    :param data_frame: e.g. "detector"
+     in which frame is defined the input data, available options:
+
+     - 'crystal' if the data was interpolated into the crystal frame using (xrayutilities)
+       or (transformation matrix + align_q=True)
+     - 'laboratory' if the data was interpolated into the laboratory frame using
+       the transformation matrix (align_q: False)
+     - 'detector' if the data is still in the detector frame
+
+    :param ref_axis_q: e.g. "y"
+     axis along which q will be aligned (data_frame= 'detector' or 'laboratory') or is
+     already aligned (data_frame='crystal')
+    :param save_frame: e.g. "laboratory"
+     in which frame should be saved the data, available options:
+
+     - 'crystal' to save the data with q aligned along ref_axis_q
+     - 'laboratory' to save the data in the laboratory frame (experimental geometry)
+     - 'lab_flat_sample' to save the data in the laboratory frame, with all sample
+       angles rotated back to 0. The rotations for 'laboratory' and 'lab_flat_sample'
+       are realized after the strain calculation (which is always done in the crystal
+       frame along ref_axis_q)
+       
+    :param isosurface_strain: e.g. 0.2
+     threshold use for removing the outer layer (the strain is undefined at the exact
+     surface voxel)
+    :param strain_method: e.g. "default"
+     how to calculate the strain, available options:
+
+     - 'default': use the single value calculated from the gradient of the phase
+     - 'defect': it will offset the phase in a loop and keep the smallest magnitude
+       value for the strain. See: F. Hofmann et al. PhysRevMaterials 4, 013801 (2020)
+
+    :param phase_offset: e.g. 0
+     manual offset to add to the phase, should be 0 in most cases
+    :param phase_offset_origin: e.g. [12, 32, 65]
+     the phase at this voxel will be set to phase_offset, leave None to use the default
+     position computed using offset_method (see below)
+    :param offset_method: e.g. "mean"
+     'com' (center of mass) or 'mean', method for determining the phase offset origin
+    :param centering_method: e.g. "max_com"
+    'com' (center of mass), 'max', 'max_com' (max then com), 'do_nothing'
+
+    Parameters for the beamline:
+
+    :param beamline: e.g. "ID01"
+     name of the beamline, used for data loading and normalization by monitor
+    :param actuators: e.g. {'rocking_angle': 'actuator_1_1'}
+     optional dictionary that can be used to define the entries corresponding to
+     actuators in data files (useful at CRISTAL where the location of data keeps
+     changing, or to declare a non-standard monitor)
+    :param is_series: e.g. True
+     specific to series measurement at P10
+    :param custom_scan: e.g. False
+     True for a stack of images acquired without scan, e.g. with ct in a
+     macro, or when there is no spec/log file available
+    :param custom_images: list of image numbers for the custom_scan, None otherwise
+    :param custom_monitor: list of monitor values for normalization for the custom_scan,
+     None otherwise
+    :param rocking_angle: e.g. "outofplane"
+     "outofplane" for a sample rotation around x outboard, "inplane" for a sample
+     rotation around y vertical up
+    :param sdd: e.g. 0.50678
+     in m, sample to detector distance in m
+    :param energy: e.g. 9000
+     X-ray energy in eV
+    :param beam_direction: e.g. [1, 0, 0]
+     beam direction in the laboratory frame (downstream, vertical up, outboard)
+    :param sample_offsets: e.g. None
+     tuple of offsets in degrees of the sample for each sample circle (outer first).
+     convention: the sample offsets will be subtracted to the motor values. Leave None
+     if there is no offset.
+    :param tilt_angle: e.g. 0.00537
+     angular step size in degrees for the rocking angle
+    :param outofplane_angle: e.g. 42.6093
+     detector angle in deg (rotation around x outboard, typically delta), corrected for
+     the direct beam position. Leave None to use the uncorrected position.
+    :param inplane_angle: e.g. -0.5783
+     detector angle in deg(rotation around y vertical up, typically gamma), corrected
+     for the direct beam position. Leave None to use the uncorrected position.
+    :param specfile_name: e.g. "l5"
+     beamline-dependent parameter, use the following template:
+
+     - template for ID01 and 34ID-C: name of the spec file without '.spec'
+     - template for SIXS: full path of the alias dictionnary or None to use the one in
+       the package folder
+     - template for all other beamlines: None
+
+    Parameters for custom scans:
+
+    :param custom_scan: e.g. False
+     True for a stack of images acquired without scan, e.g. with ct in a
+     macro, or when there is no spec/log file available
+    :param custom_motors: e.g. {"delta": 5.5, "gamma": 42.2, "theta": 1.1, "phi": 0}
+     dictionary providing the goniometer positions of the beamline
+
+    Parameters for the detector:
+
+    :param detector: e.g. "Maxipix"
+     name of the detector
+    :param pixel_size: e.g. 100e-6
+     use this to declare the pixel size of the "Dummy" detector if different from 55e-6
+    :param template_imagefile: e.g. "data_mpx4_%05d.edf.gz"
+     use one of the following template:
+
+     - template for ID01: 'data_mpx4_%05d.edf.gz' or 'align_eiger2M_%05d.edf.gz'
+     - template for SIXS_2018: 'align.spec_ascan_mu_%05d.nxs'
+     - template for SIXS_2019: 'spare_ascan_mu_%05d.nxs'
+     - template for Cristal: 'S%d.nxs'
+     - template for P10: '_master.h5'
+     - template for NANOMAX: '%06d.h5'
+     - template for 34ID: 'Sample%dC_ES_data_51_256_256.npz'
+
+    Parameters related to the refraction correction:
+
+    :param correct_refraction: e.g. True
+     True for correcting the phase shift due to refraction
+    :param optical_path_method: e.g. "threshold"
+     'threshold' or 'defect', if 'threshold' it uses isosurface_strain to define the
+     support  for the optical path calculation, if 'defect' (holes) it tries to remove
+     only outer layers even if the amplitude is lower than isosurface_strain inside
+     the crystal
+    :param dispersion: e.g. 5.0328e-05
+     delta value used for refraction correction, for Pt:  3.0761E-05 @ 10300eV,
+     5.0328E-05 @ 8170eV, 3.2880E-05 @ 9994eV, 4.1184E-05 @ 8994eV, 5.2647E-05 @ 7994eV,
+     4.6353E-05 @ 8500eV / Ge 1.4718E-05 @ 8keV
+    :param absorption: e.g. 4.1969e-06
+     beta value, for Pt:  2.0982E-06 @ 10300eV, 4.8341E-06 @ 8170eV,
+     2.3486E-06 @ 9994eV, 3.4298E-06 @ 8994eV, 5.2245E-06 @ 7994eV, 4.1969E-06 @ 8500eV
+    :param threshold_unwrap_refraction: e.g. 0.05
+     threshold used to calculate the optical path. The threshold for refraction
+     correction should be low, to correct for an object larger than the real one,
+     otherwise it messes up the phase
 """
 
 
