420270b52340b74dc5acae287c9e26db6c2d3c2a
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-11-02 22:32:39 +0100

    Update bcdi_preprocess_BCDI.py

--- scripts/preprocessing/bcdi_preprocess_BCDI.py
+++ scripts/preprocessing/bcdi_preprocess_BCDI.py
@@ -569,7 +569,7 @@ def run(prm):
         try:    
             plt.switch_backend(prm["backend"])
         except ModuleNotFoundError:
-            print(f"{prm["backend"]} backend is not supported.")
+            print(f"{prm['backend']} backend is not supported.")
 
     ###################
     # define colormap #
