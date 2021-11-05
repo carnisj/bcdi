e44166cccb3f085aad8dac0aca201a163f147806
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-11-05 21:33:31 +0100

    import package at the top of the module

--- bcdi/utils/utilities.py
+++ bcdi/utils/utilities.py
@@ -18,6 +18,7 @@ from matplotlib import pyplot as plt
 from numbers import Real, Integral
 import numpy as np
 import os
+from PIL import Image
 from scipy.interpolate import interp1d, RegularGridInterpolator
 from scipy.optimize import curve_fit
 from scipy.special import erf
@@ -827,8 +828,6 @@ def image_to_ndarray(filename, convert_grey=True, cmap=None, debug=False):
     :param debug: True to see plots
     :return:
     """
-    from PIL import Image
-
     if cmap is None:
         cmap = gu.Colormap(bad_color="1.0").cmap
 
