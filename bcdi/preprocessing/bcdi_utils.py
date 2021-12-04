2669c49055f7f4ba2c1a83b085908b68ddc37dfe
Jerome Carnis <carnis_jerome@yahoo.fr>
2021-12-04 15:15:01 +0100

    remove useless comment

--- bcdi/preprocessing/bcdi_utils.py
+++ bcdi/preprocessing/bcdi_utils.py
@@ -3,7 +3,6 @@
 # BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
 #   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
 #   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
-#   (c) 06/2021-present : DESY CFEL
 #       authors:
 #         Jerome Carnis, jerome.carnis@esrf.fr
 """Functions related to BCDI data preprocessing, before phase retrieval."""
