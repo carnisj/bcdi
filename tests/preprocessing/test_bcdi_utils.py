88003d50a18f2f2236a3bcd6c847adb854a318f8
Jerome Carnis <jerome.carnis@desy.de>
2021-12-29 21:51:56 +0100

    add unit test

--- tests/preprocessing/test_bcdi_utils.py
+++ tests/preprocessing/test_bcdi_utils.py
@@ -6,7 +6,11 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 
+import numpy as np
 import unittest
+
+from bcdi.preprocessing.bcdi_utils import find_bragg
+from bcdi.utils.utilities import gaussian_window
 
 
 def run_tests(test_class):
@@ -15,12 +19,26 @@ def run_tests(test_class):
     return runner.run(suite)
 
 
-class Test(unittest.TestCase):
-    """Tests."""
+class TestFindBragg(unittest.TestCase):
+    """
+    Tests related to the function find_bragg.
 
-    def test_dummy(self):
-        self.assertTrue(True)
+    def find_bragg(
+            data: np.ndarray,
+            peak_method: str,
+            roi: Optional[Tuple[int, int, int, int]] = None,
+            binning: Optional[Tuple[int, ...]] = None,
+    ) -> Tuple[int, ...]:
+    """
+    def setUp(self) -> None:
+        data = np.zeros((4, 32, 32))
+        data[:-1, -12:, 18:30] = gaussian_window(window_shape=(3, 12, 12))
+        self.data = data
+
+    def test_no_method(self):
+        with self.assertRaises(ValueError):
+            find_bragg(data=self.data, peak_method="wrong")
 
 
 if __name__ == "__main__":
-    run_tests(Test)
+    run_tests(TestFindBragg)
