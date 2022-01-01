eff4c23e70cd1a5e7990f106bf0dabe503144a99
Jerome Carnis <jerome.carnis@desy.de>
2022-01-01 14:09:58 +0100

    add unit test

--- tests/preprocessing/test_bcdi_utils.py
+++ tests/preprocessing/test_bcdi_utils.py
@@ -39,6 +39,46 @@ class TestFindBragg(unittest.TestCase):
         with self.assertRaises(ValueError):
             find_bragg(data=self.data, peak_method="wrong")
 
+    def test_max(self):
+        peak = find_bragg(data=self.data, peak_method="max")
+        self.assertTrue(peak == (1, 26, 24))
+
+    def test_com(self):
+        peak = find_bragg(data=self.data, peak_method="com")
+        self.assertTrue(peak == (1, 26, 24))
+
+    def test_maxcom(self):
+        peak = find_bragg(data=self.data, peak_method="maxcom")
+        self.assertTrue(peak == (1, 26, 24))
+
+    def test_binning_wrong_length(self):
+        with self.assertRaises(ValueError):
+            find_bragg(data=self.data, peak_method="max", binning=(2, 2))
+
+    def test_binning_wrong_type(self):
+        with self.assertRaises(TypeError):
+            find_bragg(data=self.data, peak_method="max", binning=(2, 2, 2.3))
+
+    def test_binning(self):
+        peak = find_bragg(data=self.data, peak_method="max", binning=(2, 1, 1))
+        self.assertTrue(peak == (2, 26, 24))
+
+    def test_binning_2(self):
+        peak = find_bragg(data=self.data, peak_method="max", binning=(3, 3, 4))
+        self.assertTrue(peak == (3, 78, 96))
+
+    def test_roi_wrong_length(self):
+        with self.assertRaises(ValueError):
+            find_bragg(data=self.data, peak_method="max", roi=(2, 1, 1))
+
+    def test_roi_wrong_type(self):
+        with self.assertRaises(TypeError):
+            find_bragg(data=self.data, peak_method="max", roi=(2, 1, 1, 1.2))
+
+    def test_roi(self):
+        peak = find_bragg(data=self.data, peak_method="max", roi=(3, 3, 4, 2))
+        self.assertTrue(peak == (1, 29, 28))
+
 
 if __name__ == "__main__":
     run_tests(TestFindBragg)
