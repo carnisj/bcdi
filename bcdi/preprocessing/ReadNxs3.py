870b758d9df4ad76097a504d6d98e426ab13051c
Ravenne <51970962+DSimonne@users.noreply.github.com>
2022-01-10 12:24:54 +0100

    Put a default alias file for ReadNxs

--- bcdi/preprocessing/ReadNxs3.py
+++ bcdi/preprocessing/ReadNxs3.py
@@ -22,7 +22,8 @@ import pickle
 import time
 from matplotlib import pyplot as plt
 import bcdi.utils.utilities as util
-
+import inspect
+import bcdi
 
 class EmptyO:
     """Empty class used as container in the nxs2spec case."""
@@ -64,6 +65,8 @@ class DataSet:
         attlist = []  # used for self generated file attribute list
         aliases = []  # used for SBS imported with alias_dict file
 
+        self.path_aliases = inspect.getfile(bcdi).split("__")[0] + "preprocessing/"
+
         print("### ReadNxs3 ###")
         if alias_dict:
             try:
@@ -81,8 +84,22 @@ class DataSet:
                 alias_dict = dirname + f"\\{dict_name}_unix.txt"
                 self._alias_dict = pickle.load(open(alias_dict, "rb"))
         else:
-            print("NO ALIAS FILE")
-            self._alias_dict = None
+            print("Defaulting to alias_dict_2021.txt")
+            alias_dict = self.path_aliases + "alias_dict_2021.txt"
+            try:
+                self._alias_dict = pickle.load(open(alias_dict, "rb"))
+            except pickle.UnpicklingError:
+                # need to convert DOS linefeeds (crlf) to UNIX (lf)
+                dirname = os.path.dirname(alias_dict)
+                dict_name = os.path.splitext(os.path.basename(alias_dict))[
+                    0
+                ]  # e.g.'alias_dict_2021'
+                util.dos2unix(
+                    input_file=alias_dict,
+                    output_file=dirname + f"\\{dict_name}_unix.txt",
+                )
+                alias_dict = dirname + f"\\{dict_name}_unix.txt"
+                self._alias_dict = pickle.load(open(alias_dict, "rb"))
 
         def is_empty(any_structure):
             """Quick function to determine if an array, tuple or string is empty."""
