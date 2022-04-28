10a61237e388c7a3249e9cf2b0235e180e1bf577
Jerome Carnis <jerome.carnis@desy.de>
2022-04-28 07:45:22 +0200

    snippets_logging: implement LoggingColor and ColorLogFormatter

--- bcdi/utils/snippets_logging.py
+++ bcdi/utils/snippets_logging.py
@@ -23,10 +23,61 @@ from logging import (
 import multiprocessing
 from typing import Optional
 
-FORMATTER = Formatter(
-    "%(asctime)s - %(levelname)s - %(name)s - %(message)s", datefmt="%m/%d/%Y %H:%M:%S"
+FILE_FORMATTER = Formatter(
+    "%(levelname)s-%(name)s: %(message)s", datefmt="%m/%d/%Y %H:%M:%S"
 )
 VALID_LEVELS = [CRITICAL, DEBUG, ERROR, INFO, WARNING]
+
+
+class LoggingColor:
+    """A class for terminal color codes."""
+
+    BOLD = "\033[1m"
+    BLUE = "\033[94m"
+    WHITE = "\033[97m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD_WHITE = BOLD + WHITE
+    BOLD_BLUE = BOLD + BLUE
+    BOLD_GREEN = BOLD + GREEN
+    BOLD_YELLOW = BOLD + YELLOW
+    BOLD_RED = BOLD + RED
+    END = "\033[0m"
+
+
+class ColorLogFormatter(Formatter):
+    """A class for formatting colored logs."""
+
+    FORMAT = "%(prefix)s%(levelname)s-%(name)s: %(message)s%(suffix)s"
+
+    LOG_LEVEL_COLOR = {
+        "DEBUG": {"prefix": LoggingColor.WHITE, "suffix": LoggingColor.END},
+        "INFO": {"prefix": LoggingColor.GREEN, "suffix": LoggingColor.END},
+        "WARNING": {"prefix": LoggingColor.BOLD_YELLOW, "suffix": LoggingColor.END},
+        "ERROR": {"prefix": LoggingColor.BOLD_RED, "suffix": LoggingColor.END},
+        "CRITICAL": {"prefix": LoggingColor.BOLD_RED, "suffix": LoggingColor.END},
+    }
+
+    def format(self, record):
+        """
+        Format log records.
+
+         It uses a default prefix and suffix to terminal color codes that corresponds
+         to the log level name.
+        """
+        if not hasattr(record, "prefix"):
+            record.prefix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
+                "prefix"
+            )
+
+        if not hasattr(record, "suffix"):
+            record.suffix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
+                "suffix"
+            )
+
+        formatter = Formatter(self.FORMAT, datefmt="%m/%d/%Y %H:%M:%S")
+        return formatter.format(record)
 
 
 def multiprocessing_logger(
@@ -47,7 +98,7 @@ def multiprocessing_logger(
     logger = multiprocessing.get_logger()
     handler = FileHandler(path, mode="w", encoding="utf-8")
     handler.setLevel(level)
-    handler.setFormatter(FORMATTER)
+    handler.setFormatter(FILE_FORMATTER)
 
     # this bit will make sure you won't have duplicated messages in the output
     if not len(logger.handlers):
@@ -73,12 +124,12 @@ def configure_logging(
     # create console and file handlers
     console_hdl = StreamHandler()
     console_hdl.setLevel(console_level)
-    console_hdl.setFormatter(FORMATTER)
+    console_hdl.setFormatter(ColorLogFormatter())
 
     if path is not None:
         file_hdl = FileHandler(path, mode="w", encoding="utf-8")
         file_hdl.setLevel(file_level)
-        file_hdl.setFormatter(FORMATTER)
+        file_hdl.setFormatter(FILE_FORMATTER)
         # configure the root logger
         basicConfig(level=DEBUG, handlers=[console_hdl, file_hdl])
     else:
