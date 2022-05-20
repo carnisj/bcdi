c2555b06cf6cca21a54b45ce7ccddb200d7080d0
Jerome Carnis <jerome.carnis@desy.de>
2022-05-20 14:37:35 +0200

    create a module constants.py

---
+++ bcdi/constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# BCDI: tools for pre(post)-processing Bragg coherent X-ray diffraction imaging data
+#   (c) 07/2017-06/2019 : CNRS UMR 7344 IM2NP
+#   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
+#       authors:
+#         Jerome Carnis, jerome.carnis@esrf.fr
+"""Constants used throughout the package."""
+
+BEAMLINES_BCDI = [
+    "ID01",
+    "ID01BLISS",
+    "SIXS_2018",
+    "SIXS_2019",
+    "34ID",
+    "P10",
+    "CRISTAL",
+    "NANOMAX",
+]
+BEAMLINES_SAXS = ["P10_SAXS", "ID27"]
