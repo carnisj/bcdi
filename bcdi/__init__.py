3916fb10db80b93318d5768554f51bd2626a97f7
Ravenne <51970962+DSimonne@users.noreply.github.com>
2022-06-15 11:37:15 +0200

    Add version function

--- bcdi/__init__.py
+++ bcdi/__init__.py
@@ -7,3 +7,22 @@
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 """The main bcdi package, which contains the whole framework."""
 __version__ = "0.2.7"
+
+
+def get_git_version():
+    """
+    Get the full version name with git hash, e.g. "2020.1-65-g958b7254-dirty"
+    Only works if the current directory is part of the git repository.
+    :return: the version name
+    """
+    from subprocess import Popen, PIPE
+    try:
+        p = Popen(['git', 'describe', '--tags', '--dirty', '--always'],
+                  stdout=PIPE, stderr=PIPE)
+        return p.stdout.readlines()[0].strip().decode("UTF-8")
+    except:
+        # in distributed & installed versions this is replaced by a string
+        __git_version_static__ = "git_version_placeholder"
+        if "placeholder" in __git_version_static__:
+            return __version__
+        return __git_version_static__
