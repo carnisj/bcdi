a44e45df5150487ee389f3d7824e61ca0052e881
carnisj <carnis_jerome@yahoo.fr>
2022-06-17 21:00:59 +0200

    add type hint, refactor docstring

--- bcdi/__init__.py
+++ bcdi/__init__.py
@@ -8,22 +8,26 @@
 """The main bcdi package, which contains the whole framework."""
 __version__ = "0.2.7"
 
+from subprocess import PIPE, Popen
 
-def get_git_version():
+
+def get_git_version() -> str:
     """
-    Get the full version name with git hash, e.g. "2020.1-65-g958b7254-dirty"
+    Get the full version name with git hash, e.g. "2020.1-65-g958b7254-dirty".
+
     Only works if the current directory is part of the git repository.
+
     :return: the version name
     """
-    from subprocess import Popen, PIPE
-
     try:
         p = Popen(
             ["git", "describe", "--tags", "--dirty", "--always"],
             stdout=PIPE,
             stderr=PIPE,
         )
-        return p.stdout.readlines()[0].strip().decode("UTF-8")
+        if p.stdout is not None:
+            return p.stdout.readlines()[0].strip().decode("UTF-8")
+        raise IndexError
     except IndexError:
         # in distributed & installed versions this is replaced by a string
         __git_version_static__ = "git_version_placeholder"
