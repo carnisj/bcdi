0ed526314c7c3c038962965c23540855fb1b6eb9
carnisj <carnis_jerome@yahoo.fr>
2022-11-04 18:25:04 +0100

    add unit tests

--- tests/preprocessing/test_analysis.py
+++ tests/preprocessing/test_analysis.py
@@ -5,19 +5,13 @@
 #   (c) 07/2019-05/2021 : DESY PHOTON SCIENCE
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
-import os.path
-import tempfile
+
 import unittest
-from copy import deepcopy
-from logging import Logger
 from pathlib import Path
-from unittest.mock import patch
 
 import matplotlib
-import numpy as np
 
 import bcdi.preprocessing.analysis as analysis
-from bcdi.experiment.setup import Setup
 from bcdi.preprocessing.preprocessing_runner import initialize_parameters_bcdi
 from bcdi.utils.parser import ConfigParser
 from tests.config import run_tests
@@ -32,3 +26,40 @@ try:
     skip_tests = False
 except ValueError:
     skip_tests = True
+
+
+class TestDefineAnalysisType(unittest.TestCase):
+    def test_reload_orthogonal_true(self) -> None:
+        self.assertTrue(
+            analysis.define_analysis_type(
+                reload_orthogonal=True, use_rawdata=True, interpolation_method=""
+            ),
+            "interpolated",
+        )
+        self.assertTrue(
+            analysis.define_analysis_type(
+                reload_orthogonal=True, use_rawdata=False, interpolation_method=""
+            ),
+            "interpolated",
+        )
+
+    def test_reload_orthogonal_false_use_rawdata(self) -> None:
+        self.assertTrue(
+            analysis.define_analysis_type(
+                reload_orthogonal=False, use_rawdata=True, interpolation_method=""
+            ),
+            "detector_frame",
+        )
+
+    def test_reload_orthogonal_false_interpolate_rawdata(self) -> None:
+        method = "this_method"
+        self.assertTrue(
+            analysis.define_analysis_type(
+                reload_orthogonal=False, use_rawdata=False, interpolation_method=method
+            ),
+            method,
+        )
+
+
+if __name__ == "__main__":
+    run_tests(TestDefineAnalysisType)
