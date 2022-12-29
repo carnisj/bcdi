bc26cbe8f1d76dd67607c9df1e54334e2f5314ec
carnisj <carnis_jerome@yahoo.fr>
2022-12-29 20:31:09 +0100

    prepare for version 0.2.9

--- bcdi/__init__.py
+++ bcdi/__init__.py
@@ -6,7 +6,7 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 """The main bcdi package, which contains the whole framework."""
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 from subprocess import PIPE, Popen
 
