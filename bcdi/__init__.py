733890a76a692298df541a1a0c569caabf4d6ae8
carnisj <44344327+carnisj@users.noreply.github.com>
2024-01-23 21:20:29 +0100

    Update version

--- bcdi/__init__.py
+++ bcdi/__init__.py
@@ -6,7 +6,7 @@
 #       authors:
 #         Jerome Carnis, carnis_jerome@yahoo.fr
 """The main bcdi package, which contains the whole framework."""
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 from subprocess import PIPE, Popen
 
